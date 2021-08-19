d6c694bb32056767b03a9bcaa3b89995cfba808c
Paolo Pepito <npaolopepito@gmail.com>
2021-08-19 12:40:16 +0800

    Incorporate base code from google_maps_flutter in pub.dev

--- lib/logic/home/home_view_model.dart
+++ lib/logic/home/home_view_model.dart
@@ -1,1 +1,32 @@
-class HomeViewModel {}
+import 'dart:async';
+
+import 'package:google_maps_flutter/google_maps_flutter.dart';
+import 'package:hooks_riverpod/hooks_riverpod.dart';
+
+final homeViewModel = Provider.autoDispose((ref) => HomeViewModel(ref.read));
+
+class HomeViewModel {
+  HomeViewModel(this.read);
+  final Reader read;
+
+  final Completer<GoogleMapController> _controller = Completer();
+
+  final CameraPosition kGooglePlex = const CameraPosition(
+    target: LatLng(37.42796133580664, -122.085749655962),
+    zoom: 14.4746,
+  );
+
+  static const CameraPosition kLake = CameraPosition(
+      bearing: 192.8334901395799,
+      target: LatLng(10.367836951212034, 123.91396789976241),
+      tilt: 59.440717697143555,
+      zoom: 19.151926040649414);
+
+  void onMapCreated(GoogleMapController gmapController) =>
+      _controller.complete(gmapController);
+
+  Future<void> goToLake() async {
+    final GoogleMapController controller = await _controller.future;
+    controller.animateCamera(CameraUpdate.newCameraPosition(kLake));
+  }
+}
--- lib/ui/home/home_view.dart
+++ lib/ui/home/home_view.dart
@@ -1,15 +1,25 @@
 import 'package:flutter/material.dart';
+import 'package:google_maps_flutter/google_maps_flutter.dart';
+import 'package:hooks_riverpod/hooks_riverpod.dart';
+import 'package:kapiot/logic/home/home_view_model.dart';
 
-class HomeView extends StatelessWidget {
+class HomeView extends HookConsumerWidget {
   const HomeView({Key? key}) : super(key: key);
 
   @override
-  Widget build(BuildContext context) {
+  Widget build(BuildContext context, WidgetRef ref) {
+    final model = ref.watch(homeViewModel);
     return LayoutBuilder(
       builder: (context, constraints) {
         return Scaffold(
-          body: Container(
-            color: Colors.amber,
+          body: GoogleMap(
+            mapType: MapType.hybrid,
+            initialCameraPosition: model.kGooglePlex,
+            onMapCreated: model.onMapCreated,
+          ),
+          floatingActionButton: FloatingActionButton.extended(
+            onPressed: model.goToLake,
+            label: const Text('Balay sa gwapo 😎'),
           ),
         );
       },
