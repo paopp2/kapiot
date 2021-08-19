9c06f9e06a0c7214f3296ce2413ac8007d1eecf3
Charles Jr Ausejo <charlesjrausejo@gmail.com>
2021-08-19 13:37:13 +0800

    Add google sign in inside LoginViewModel

--- lib/logic/auth/login_view_model.dart
+++ lib/logic/auth/login_view_model.dart
@@ -1,11 +1,19 @@
 import 'package:hooks_riverpod/hooks_riverpod.dart';
 import 'package:kapiot/app_router.dart';
+import 'package:kapiot/data/services/auth_service.dart';
 
-final loginViewModel = Provider.autoDispose((ref) => LoginViewModel(ref.read));
+final loginViewModel = Provider.autoDispose((ref) => LoginViewModel(
+      read: ref.read,
+      authService: ref.watch(authServiceProvider),
+    ));
 
 class LoginViewModel {
-  LoginViewModel(this.read);
+  LoginViewModel({required this.read, required this.authService});
   final Reader read;
+  final AuthService authService;
 
+  void initState() {}
+  Future<void> signInWithGoogle() async => await authService.signInWithGoogle();
   void gotoHomeView() => AppRouter.instance.navigateTo(Routes.homeView);
+  void dispose() {}
 }
