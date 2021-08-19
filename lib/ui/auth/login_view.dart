0a6794736951fbbbd198f695bb69c05fc6b4faee
Paolo Pepito <npaolopepito@gmail.com>
2021-08-19 09:53:52 +0800

    Add ui/ directory with a LoginView file

---
+++ lib/ui/auth/login_view.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+
+class LoginView extends StatelessWidget {
+  const LoginView({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return LayoutBuilder(
+      builder: (context, constraints) {
+        return Scaffold(
+          body: Container(
+            color: Colors.green,
+          ),
+        );
+      },
+    );
+  }
+}
