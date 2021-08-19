595cb699b8d842b674abd0578f0929ce5255f83f
Paolo Pepito <npaolopepito@gmail.com>
2021-08-19 14:28:43 +0800

    Add initial LocationService
    This commit configures HomeViewModel to initialize the map to the
    device's current location using the LocationService

--- lib/logic/home/home_view_model.dart
+++ lib/logic/home/home_view_model.dart
@@ -2,31 +2,50 @@ import 'dart:async';
 
 import 'package:google_maps_flutter/google_maps_flutter.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
+import 'package:kapiot/data/services/location_service.dart';
+import 'package:kapiot/logic/home/home_view_state.dart';
 
-final homeViewModel = Provider.autoDispose((ref) => HomeViewModel(ref.read));
+final homeViewModelProvider = Provider.autoDispose(
+  (ref) => HomeViewModel(
+    read: ref.read,
+    locationService: ref.watch(locationServiceProvider),
+  ),
+);
 
 class HomeViewModel {
-  HomeViewModel(this.read);
+  HomeViewModel({
+    required this.read,
+    required this.locationService,
+  });
   final Reader read;
-
+  final LocationService locationService;
   final Completer<GoogleMapController> _controller = Completer();
+  late final CameraPosition initialPosition;
 
-  final CameraPosition kGooglePlex = const CameraPosition(
-    target: LatLng(37.42796133580664, -122.085749655962),
-    zoom: 14.4746,
-  );
-
-  static const CameraPosition kLake = CameraPosition(
+  static const CameraPosition gwapoHouse = CameraPosition(
       bearing: 192.8334901395799,
       target: LatLng(10.367836951212034, 123.91396789976241),
       tilt: 59.440717697143555,
       zoom: 19.151926040649414);
 
+  Future<void> initState() async {
+    final currentLoc = await locationService.getLocation();
+    read(startLocationProvider).state = currentLoc;
+    initialPosition = CameraPosition(
+      target: LatLng(currentLoc.latitude, currentLoc.longitude),
+      bearing: 192.8334901395799,
+      tilt: 59.440717697143555,
+      zoom: 19.151926040649414,
+    );
+  }
+
+  void dispose() {}
+
   void onMapCreated(GoogleMapController gmapController) =>
       _controller.complete(gmapController);
 
-  Future<void> goToLake() async {
+  Future<void> goToGwapoHouse() async {
     final GoogleMapController controller = await _controller.future;
-    controller.animateCamera(CameraUpdate.newCameraPosition(kLake));
+    controller.animateCamera(CameraUpdate.newCameraPosition(gwapoHouse));
   }
 }
--- lib/ui/home/home_view.dart
+++ lib/ui/home/home_view.dart
@@ -1,26 +1,45 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_hooks/flutter_hooks.dart';
 import 'package:google_maps_flutter/google_maps_flutter.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
 import 'package:kapiot/logic/home/home_view_model.dart';
+import 'package:kapiot/logic/home/home_view_state.dart';
 
 class HomeView extends HookConsumerWidget {
   const HomeView({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context, WidgetRef ref) {
-    final model = ref.watch(homeViewModel);
+    final model = ref.watch(homeViewModelProvider);
+    final startLocation = ref.watch(startLocationProvider).state;
+
+    useEffect(() {
+      model.initState();
+      return model.dispose;
+    }, []);
+
     return LayoutBuilder(
       builder: (context, constraints) {
         return Scaffold(
-          body: GoogleMap(
-            mapType: MapType.hybrid,
-            initialCameraPosition: model.kGooglePlex,
-            onMapCreated: model.onMapCreated,
-          ),
           floatingActionButton: FloatingActionButton.extended(
-            onPressed: model.goToLake,
+            onPressed: model.goToGwapoHouse,
             label: const Text('Balay sa gwapo 😎'),
           ),
+          body: (startLocation == null)
+              ? Center(
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: const [
+                      CircularProgressIndicator(),
+                      Text("Fetching current location..."),
+                    ],
+                  ),
+                )
+              : GoogleMap(
+                  mapType: MapType.hybrid,
+                  initialCameraPosition: model.initialPosition,
+                  onMapCreated: model.onMapCreated,
+                ),
         );
       },
     );
