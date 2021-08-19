1205442bddf925c1621e19059aa34d0c2880d39d
Paolo Pepito <npaolopepito@gmail.com>
2021-08-19 09:56:40 +0800

    Add a home/ directory for HomeView and components

---
+++ lib/ui/home/home_view.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+
+class HomeView extends StatelessWidget {
+  const HomeView({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return LayoutBuilder(
+      builder: (context, constraints) {
+        return Scaffold(
+          body: Container(
+            color: Colors.amber,
+          ),
+        );
+      },
+    );
+  }
+}
