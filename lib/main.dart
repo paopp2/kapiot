e754a4b695ca217b30df33562682db6a8081e46d
Paolo Pepito <npaolopepito@gmail.com>
2021-08-19 10:27:53 +0800

    Add ProviderScope in main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,10 @@
 import 'package:flutter/material.dart';
+import 'package:hooks_riverpod/hooks_riverpod.dart';
 import 'package:kapiot/app_router.dart';
 import 'package:kapiot/ui/auth/login_view.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp(const ProviderScope(child: MyApp()));
 }
 
 class MyApp extends StatelessWidget {
