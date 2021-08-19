64db2538ac2b4020d235470e01d928f0901949e3
Charles Jr Ausejo <charlesjrausejo@gmail.com>
2021-08-19 13:31:39 +0800

    Fix auth provider error.

--- lib/data/core_providers/auth_providers.dart
+++ lib/data/core_providers/auth_providers.dart
@@ -3,8 +3,7 @@ import 'package:kapiot/data/core_providers/firebase_providers.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 
 final authStateChangesProvider = StreamProvider<User?>(
-  (ref) => ref.watch(fireauthProvider).authStateChanges();
-);
+    (ref) => ref.watch(fireauthProvider).authStateChanges());
 
 // TODO: Create currentUserProvider 
 
