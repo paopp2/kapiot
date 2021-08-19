879ff932ae62de5ffc5c10c54326e25aaaaf6a57
Charles Jr Ausejo <charlesjrausejo@gmail.com>
2021-08-19 14:15:13 +0800

    Change HomeView to HookConsumerWidget

--- lib/ui/home/home_view.dart
+++ lib/ui/home/home_view.dart
@@ -1,15 +1,32 @@
 import 'package:flutter/material.dart';
+import 'package:hooks_riverpod/hooks_riverpod.dart';
 
-class HomeView extends StatelessWidget {
+class HomeView extends HookConsumerWidget {
   const HomeView({Key? key}) : super(key: key);
 
   @override
-  Widget build(BuildContext context) {
+  Widget build(BuildContext context, WidgetRef ref) {
     return LayoutBuilder(
       builder: (context, constraints) {
         return Scaffold(
           body: Container(
             color: Colors.amber,
+            child: Center(
+              child: Padding(
+                padding: EdgeInsets.all(18.0),
+                child: ElevatedButton.icon(
+                  style: ElevatedButton.styleFrom(
+                    primary: Colors.white,
+                    onPrimary: Colors.black,
+                    minimumSize: const Size(double.infinity, 50),
+                  ),
+                  // icon: const Icon(FontAwesomeIcons.google),
+                  icon: const Icon(Icons.exit_to_app_outlined),
+                  label: const Text('Sign Out'),
+                  onPressed: () {},
+                ),
+              ),
+            ),
           ),
         );
       },
