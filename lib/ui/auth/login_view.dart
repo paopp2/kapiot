895299ee239062120d8e7ed6d5b234a6dc607fd9
Charles Jr Ausejo <charlesjrausejo@gmail.com>
2021-08-19 14:07:41 +0800

    Add authentication between login and home view

--- lib/ui/auth/login_view.dart
+++ lib/ui/auth/login_view.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
 import 'package:kapiot/logic/auth/login_view_model.dart';
+import 'package:flutter_hooks/flutter_hooks.dart';
 
 class LoginView extends HookConsumerWidget {
   const LoginView({Key? key}) : super(key: key);
@@ -8,15 +9,35 @@ class LoginView extends HookConsumerWidget {
   @override
   Widget build(BuildContext context, WidgetRef ref) {
     final model = ref.watch(loginViewModel);
+
+    useEffect(() {
+      model.initState();
+      return model.dispose;
+    }, []);
+
     return LayoutBuilder(
       builder: (context, constraints) {
         return Scaffold(
-          floatingActionButton: FloatingActionButton.extended(
-            label: const Text("Go to HomeView"),
-            onPressed: model.gotoHomeView,
-          ),
-          body: Container(
-            color: Colors.green,
+          body: Padding(
+            padding: EdgeInsets.all(16.0),
+            child: Center(
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  ElevatedButton.icon(
+                    style: ElevatedButton.styleFrom(
+                      primary: Colors.white,
+                      onPrimary: Colors.black,
+                      minimumSize: const Size(double.infinity, 50),
+                    ),
+                    // icon: const Icon(FontAwesomeIcons.google),
+                    icon: const Icon(Icons.enhanced_encryption_outlined),
+                    label: const Text('Sign In with Google'),
+                    onPressed: model.signInWithGoogle,
+                  ),
+                ],
+              ),
+            ),
           ),
         );
       },
