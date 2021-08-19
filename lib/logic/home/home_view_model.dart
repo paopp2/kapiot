964fa46e44b1ee7f68819a6a29316634cc1de342
Charles Jr Ausejo <charlesjrausejo@gmail.com>
2021-08-19 14:20:47 +0800

    Add HomeViewModel functionality

--- lib/logic/home/home_view_model.dart
+++ lib/logic/home/home_view_model.dart
@@ -1,1 +1,17 @@
-class HomeViewModel {}
+import 'package:hooks_riverpod/hooks_riverpod.dart';
+import 'package:kapiot/data/services/auth_service.dart';
+
+final homeViewModel = Provider.autoDispose((ref) => HomeViewModel(
+      read: ref.read,
+      authService: ref.watch(authServiceProvider),
+    ));
+
+class HomeViewModel {
+  HomeViewModel({required this.read, required this.authService});
+  final Reader read;
+  final AuthService authService;
+
+  void initState() {}
+  Future<void> signOut() async => await authService.signOutGoogle();
+  void dispose() {}
+}
