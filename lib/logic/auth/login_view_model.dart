526e89b196c05beb0e2724cd0325d002ea39067b
Paolo Pepito <npaolopepito@gmail.com>
2021-08-19 10:30:10 +0800

    Add basic model method for LoginView
    Method to navigate to HomeView through FAB

--- lib/logic/auth/login_view_model.dart
+++ lib/logic/auth/login_view_model.dart
@@ -1,1 +1,11 @@
-class LoginViewModel {}
+import 'package:hooks_riverpod/hooks_riverpod.dart';
+import 'package:kapiot/app_router.dart';
+
+final loginViewModel = Provider.autoDispose((ref) => LoginViewModel(ref.read));
+
+class LoginViewModel {
+  LoginViewModel(this.read);
+  final Reader read;
+
+  void gotoHomeView() => AppRouter.instance.navigateTo(Routes.homeView);
+}
--- lib/ui/auth/login_view.dart
+++ lib/ui/auth/login_view.dart
@@ -1,13 +1,20 @@
 import 'package:flutter/material.dart';
+import 'package:hooks_riverpod/hooks_riverpod.dart';
+import 'package:kapiot/logic/auth/login_view_model.dart';
 
-class LoginView extends StatelessWidget {
+class LoginView extends HookConsumerWidget {
   const LoginView({Key? key}) : super(key: key);
 
   @override
-  Widget build(BuildContext context) {
+  Widget build(BuildContext context, WidgetRef ref) {
+    final model = ref.watch(loginViewModel);
     return LayoutBuilder(
       builder: (context, constraints) {
         return Scaffold(
+          floatingActionButton: FloatingActionButton.extended(
+            label: const Text("Go to HomeView"),
+            onPressed: model.gotoHomeView,
+          ),
           body: Container(
             color: Colors.green,
           ),
