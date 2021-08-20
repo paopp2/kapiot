5ad01cfb4a769093de92e28238fe759e8c49e0cf
crisxchan <chrisgonzales.online@gmail.com>
2021-08-20 20:50:41 +0800

    Add basic UI for RequestAcceptedView

--- lib/ui/rider/request_accepted_view.dart
+++ lib/ui/rider/request_accepted_view.dart
@@ -10,9 +10,198 @@ class RequestAcceptedView extends StatelessWidget {
         return Scaffold(
           body: Container(
             color: Colors.yellow,
+            child: Column(
+              children: [
+                SizedBox(
+                  height: 400,
+                  width: double.infinity,
+                ),
+                Expanded(
+                  child: Stack(
+                    children: [
+                      Container(
+                        width: double.infinity,
+                        decoration: BoxDecoration(
+                          color: Colors.white,
+                          borderRadius: BorderRadius.only(
+                            topLeft: Radius.circular(35),
+                            topRight: Radius.circular(35),
+                          ),
+                        ),
+                        child: Container(
+                          margin: EdgeInsets.all(10),
+                          child: Column(
+                            children: [
+                              SizedBox(
+                                height: 10,
+                              ),
+                              CircleAvatar(
+                                radius: 40,
+                                backgroundColor: Colors.blue,
+                              ),
+                              SizedBox(
+                                height: 10,
+                              ),
+                              Text(
+                                'Paolo Pepito',
+                              )
+                            ],
+                          ),
+                        ),
+                      ),
+                      Container(
+                        child: Align(
+                          alignment: Alignment.bottomCenter,
+                          child: Container(
+                            width: double.infinity,
+                            height: 190,
+                            decoration: BoxDecoration(
+                              color: Colors.green,
+                              borderRadius: BorderRadius.only(
+                                topLeft: Radius.circular(35),
+                                topRight: Radius.circular(35),
+                              ),
+                            ),
+                            child: Column(
+                              children: [
+                                SizedBox(
+                                  height: 10,
+                                ),
+                                Text("Your co passengers for this ride"),
+                                Expanded(
+                                  child: ListView(
+                                    scrollDirection: Axis.horizontal,
+                                    padding:
+                                        EdgeInsets.symmetric(horizontal: 10),
+                                    children: [
+                                      Container(
+                                        margin:
+                                            EdgeInsets.symmetric(horizontal: 5),
+                                        child: CircleAvatar(
+                                          radius: 30,
+                                          backgroundColor: Colors.blue,
+                                        ),
+                                      ),
+                                      Container(
+                                        margin:
+                                            EdgeInsets.symmetric(horizontal: 5),
+                                        child: CircleAvatar(
+                                          radius: 30,
+                                          backgroundColor: Colors.blue,
+                                        ),
+                                      ),
+                                    ],
+                                  ),
+                                ),
+                                Container(
+                                  width: double.infinity,
+                                  height: 90,
+                                  decoration: BoxDecoration(
+                                    color: Colors.white,
+                                    borderRadius: BorderRadius.only(
+                                      topLeft: Radius.circular(35),
+                                      topRight: Radius.circular(35),
+                                    ),
+                                  ),
+                                  child: Column(
+                                    children: [
+                                      Text("Rider's reviews"),
+                                    ],
+                                  ),
+                                ),
+                              ],
+                            ),
+                          ),
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+              ],
+            ),
           ),
         );
       },
     );
   }
 }
+
+// Column(
+//                 children: [
+//                   SizedBox(
+//                     height: 400,
+//                     width: double.infinity,
+//                   ),
+//                   Expanded(
+//                     child: Container(
+//                       decoration: BoxDecoration(
+//                         color: Colors.white,
+//                         borderRadius: BorderRadius.only(
+//                           topLeft: Radius.circular(35),
+//                           topRight: Radius.circular(35),
+//                         ),
+//                       ),
+//                       child: Column(
+//                         children: [
+//                           SizedBox(
+//                             height: 30,
+//                             width: 50,
+//                             child: Divider(
+//                               color: Colors.teal,
+//                             ),
+//                           ),
+//                           Container(
+//                             width: double.infinity,
+//                             color: Colors.teal.shade100,
+//                             margin: EdgeInsets.all(10),
+//                             child: Column(
+//                               children: [
+//                                 SizedBox(
+//                                   height: 10,
+//                                 ),
+//                                 CircleAvatar(
+//                                   radius: 40,
+//                                   backgroundColor: Colors.blue,
+//                                 ),
+//                                 SizedBox(
+//                                   height: 10,
+//                                 ),
+//                                 Text(
+//                                   'Paolo Pepito',
+//                                 ),
+//                               ],
+//                             ),
+//                           ),
+//                           Expanded(
+//                             child: Container(
+//                               decoration: BoxDecoration(
+//                                 color: Colors.grey,
+//                                 borderRadius: BorderRadius.only(
+//                                   topLeft: Radius.circular(35),
+//                                   topRight: Radius.circular(35),
+//                                 ),
+//                               ),
+//                               child: Column(
+//                                 children: [
+//                                   Text("Your co-passengers for today"),
+//                                   Expanded(
+//                                     child: ListView(
+//                                       scrollDirection: Axis.horizontal,
+//                                       children: [
+//                                         CircleAvatar(
+//                                           radius: 40,
+//                                           backgroundColor: Colors.blue,
+//                                         ),
+//                                       ],
+//                                     ),
+//                                   )
+//                                 ],
+//                               ),
+//                             ),
+//                           ),
+//                         ],
+//                       ),
+//                     ),
+//                   )
+//                 ],
+//               ),
