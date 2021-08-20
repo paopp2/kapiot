57a25675337477165e0a1a0d2505d8df13cb40b9
Charles Jr Ausejo <charlesjrausejo@gmail.com>
2021-08-20 11:52:47 +0800

    Add temporary rider manager view provider

--- lib/logic/driver/rider_manager_view_model.dart
+++ lib/logic/driver/rider_manager_view_model.dart
@@ -1,1 +1,19 @@
-class RiderManagerViewModel {}
+import 'package:hooks_riverpod/hooks_riverpod.dart';
+
+// TODO: Add relevant providers
+final riderManagerViewModel =
+    Provider.autoDispose((ref) => RiderManagerViewModel(
+          read: ref.read,
+        ));
+
+// TODO: Add constructors to relevant providers
+class RiderManagerViewModel {
+  RiderManagerViewModel({required this.read});
+  final Reader read;
+
+  void initState() {}
+  // *! Temporary list to maintain provider logic
+  var riderList = ['Christian Gonzales, Blithe Gonzales', 'Charles Ausejo'];
+
+  void dispose() {}
+}
