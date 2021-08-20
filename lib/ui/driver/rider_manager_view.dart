74d14894b1becb6d7e00e7b7a9a612f438c734b3
Charles Jr Ausejo <charlesjrausejo@gmail.com>
2021-08-20 11:55:40 +0800

    Change RiderManagerView to a HookConsumerWidget

--- lib/ui/driver/rider_manager_view.dart
+++ lib/ui/driver/rider_manager_view.dart
@@ -1,10 +1,13 @@
 import 'package:flutter/material.dart';
+import 'package:hooks_riverpod/hooks_riverpod.dart';
+import 'package:kapiot/logic/driver/rider_manager_view_model.dart';
 
-class RiderManagerView extends StatelessWidget {
+class RiderManagerView extends HookConsumerWidget {
   const RiderManagerView({Key? key}) : super(key: key);
 
   @override
-  Widget build(BuildContext context) {
+  Widget build(BuildContext context, WidgetRef ref) {
+    final model = ref.watch(riderManagerViewModel);
     return LayoutBuilder(
       builder: (context, constraints) {
         return Scaffold(
