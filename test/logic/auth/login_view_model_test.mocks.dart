42a42b0459664963dfbda7f3c9712278c86ed5b8
Paolo Pepito <npaolopepito@gmail.com>
2021-08-21 23:51:11 +0800

    Generated updates for login_view_model_test from mockito

--- test/logic/auth/login_view_model_test.mocks.dart
+++ test/logic/auth/login_view_model_test.mocks.dart
@@ -2,14 +2,14 @@
 // in kapiot/test/logic/auth/login_view_model_test.dart.
 // Do not manually edit this file.
 
-import 'dart:async' as _i7;
+import 'dart:async' as _i5;
 
 import 'package:firebase_auth/firebase_auth.dart' as _i8;
 import 'package:flutter/material.dart' as _i1;
 import 'package:google_sign_in/google_sign_in.dart' as _i3;
-import 'package:hooks_riverpod/hooks_riverpod.dart' as _i6;
+import 'package:hooks_riverpod/hooks_riverpod.dart' as _i7;
 import 'package:kapiot/app_router.dart' as _i4;
-import 'package:kapiot/data/services/auth_service.dart' as _i5;
+import 'package:kapiot/data/services/auth_service.dart' as _i6;
 import 'package:mockito/mockito.dart' as _i2;
 
 // ignore_for_file: avoid_redundant_argument_values
@@ -44,10 +44,9 @@ class MockAppRouter extends _i2.Mock implements _i4.AppRouter {
               returnValue: <String, _i1.Widget Function(_i1.BuildContext)>{})
           as Map<String, _i1.Widget Function(_i1.BuildContext)>);
   @override
-  Map<String, _i1.Widget Function(_i1.BuildContext)> getRoutes() =>
-      (super.noSuchMethod(Invocation.method(#getRoutes, []),
-              returnValue: <String, _i1.Widget Function(_i1.BuildContext)>{})
-          as Map<String, _i1.Widget Function(_i1.BuildContext)>);
+  _i5.Future<dynamic>? navigateTo(String? route) =>
+      (super.noSuchMethod(Invocation.method(#navigateTo, [route]))
+          as _i5.Future<dynamic>?);
   @override
   void popView() => super.noSuchMethod(Invocation.method(#popView, []),
       returnValueForMissingStub: null);
@@ -58,33 +57,33 @@ class MockAppRouter extends _i2.Mock implements _i4.AppRouter {
 /// A class which mocks [AuthService].
 ///
 /// See the documentation for Mockito's code generation for more information.
-class MockAuthService extends _i2.Mock implements _i5.AuthService {
+class MockAuthService extends _i2.Mock implements _i6.AuthService {
   MockAuthService() {
     _i2.throwOnMissingStub(this);
   }
 
   @override
-  _i6.Reader get read => (super.noSuchMethod(Invocation.getter(#read),
-      returnValue: <T>(_i6.ProviderBase<T> provider) => null) as _i6.Reader);
+  _i7.Reader get read => (super.noSuchMethod(Invocation.getter(#read),
+      returnValue: <T>(_i7.ProviderBase<T> provider) => null) as _i7.Reader);
   @override
   _i3.GoogleSignIn get googleSignIn =>
       (super.noSuchMethod(Invocation.getter(#googleSignIn),
           returnValue: _FakeGoogleSignIn_1()) as _i3.GoogleSignIn);
   @override
-  _i7.Future<_i8.UserCredential?> signInWithGoogle() =>
+  _i5.Future<_i8.UserCredential?> signInWithGoogle() =>
       (super.noSuchMethod(Invocation.method(#signInWithGoogle, []),
               returnValue: Future<_i8.UserCredential?>.value())
-          as _i7.Future<_i8.UserCredential?>);
+          as _i5.Future<_i8.UserCredential?>);
   @override
-  _i7.Future<void> signOutGoogle() =>
+  _i5.Future<void> signOutGoogle() =>
       (super.noSuchMethod(Invocation.method(#signOutGoogle, []),
           returnValue: Future<void>.value(),
-          returnValueForMissingStub: Future<void>.value()) as _i7.Future<void>);
+          returnValueForMissingStub: Future<void>.value()) as _i5.Future<void>);
   @override
-  _i7.Future<_i8.AuthCredential?> googleCredentials() =>
+  _i5.Future<_i8.AuthCredential?> googleCredentials() =>
       (super.noSuchMethod(Invocation.method(#googleCredentials, []),
               returnValue: Future<_i8.AuthCredential?>.value())
-          as _i7.Future<_i8.AuthCredential?>);
+          as _i5.Future<_i8.AuthCredential?>);
   @override
   String toString() => super.toString();
 }
