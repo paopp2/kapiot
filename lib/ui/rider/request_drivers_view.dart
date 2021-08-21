245ec66c72f2897c9346168ddd2dc56ca790feeb
crisxchan <chrisgonzales.online@gmail.com>
2021-08-21 15:11:51 +0800

    Change bottom container to sliding up panel

--- lib/ui/rider/request_drivers_view.dart
+++ lib/ui/rider/request_drivers_view.dart
@@ -1,112 +1,30 @@
 import 'package:flutter/material.dart';
+import 'package:sliding_up_panel/sliding_up_panel.dart';
 
 class RequestDriversView extends StatelessWidget {
   const RequestDriversView({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
+    final panelHeightClosed = MediaQuery.of(context).size.height * 0.1;
+    final panelHeightOpened = MediaQuery.of(context).size.height * 0.55;
+
     return LayoutBuilder(
       builder: (context, constraints) {
         return Scaffold(
-          body: Container(
-            color: Colors.blueGrey,
-            child: Column(
-              children: [
-                const SizedBox(
-                  height: 400,
-                  width: double.infinity,
-                ),
-                Expanded(
-                  child: Container(
-                    decoration: const BoxDecoration(
-                      color: Colors.white,
-                      borderRadius: BorderRadius.only(
-                        topLeft: Radius.circular(35),
-                        topRight: Radius.circular(35),
-                      ),
-                    ),
-                    child: Column(
-                      children: [
-                        const SizedBox(
-                          height: 30,
-                          width: 50,
-                          child: Divider(
-                            color: Colors.teal,
-                          ),
-                        ),
-                        const SizedBox(
-                          height: 20,
-                        ),
-                        const Text("We found n drivers for your request "),
-                        Expanded(
-                          child: ListView(
-                            padding: const EdgeInsets.all(20),
-                            scrollDirection: Axis.horizontal,
-                            children: [
-                              Container(
-                                width: 240,
-                                color: Colors.teal.shade100,
-                                margin: const EdgeInsets.all(10),
-                                child: Column(
-                                  children: [
-                                    const SizedBox(height: 10),
-                                    const CircleAvatar(
-                                      radius: 40,
-                                      backgroundColor: Colors.blue,
-                                    ),
-                                    const SizedBox(height: 10),
-                                    const Text('Paolo Pepito'),
-                                    const SizedBox(height: 70),
-                                    Container(
-                                      height: 35,
-                                      width: 180,
-                                      decoration: const BoxDecoration(
-                                        color: Colors.blue,
-                                        borderRadius: BorderRadius.all(
-                                            Radius.circular(5)),
-                                      ),
-                                      child: const Center(
-                                          child: Text("Hail ride")),
-                                    )
-                                  ],
-                                ),
-                              ),
-                              Container(
-                                width: 240,
-                                color: Colors.teal.shade100,
-                                margin: const EdgeInsets.all(10),
-                                child: Column(
-                                  children: [
-                                    const SizedBox(height: 10),
-                                    const CircleAvatar(
-                                      radius: 40,
-                                      backgroundColor: Colors.blue,
-                                    ),
-                                    const SizedBox(height: 10),
-                                    const Text('Charles Ausejo'),
-                                    const SizedBox(height: 70),
-                                    Container(
-                                      height: 35,
-                                      width: 180,
-                                      decoration: const BoxDecoration(
-                                        color: Colors.blue,
-                                        borderRadius: BorderRadius.all(
-                                            Radius.circular(5)),
-                                      ),
-                                      child: const Center(
-                                          child: Text("Hail ride")),
-                                    )
-                                  ],
-                                ),
-                              ),
-                            ],
-                          ),
-                        ),
-                      ],
-                    ),
-                  ),
-                )
-              ],
+          body: SlidingUpPanel(
+            minHeight: panelHeightClosed,
+            maxHeight: panelHeightOpened,
+            parallaxEnabled: true,
+            parallaxOffset: 5,
+            borderRadius: BorderRadius.vertical(
+              top: Radius.circular(20),
+            ),
+            body: Expanded(
+              child: Container(),
+            ),
+            panelBuilder: (controller) => PanelWidget(
+              controller: controller,
             ),
           ),
         );
@@ -114,3 +32,129 @@ class RequestDriversView extends StatelessWidget {
     );
   }
 }
+
+class PanelWidget extends StatelessWidget {
+  final ScrollController controller;
+
+  const PanelWidget({Key? key, required this.controller}) : super(key: key);
+  @override
+  Widget build(BuildContext context) => ListView(
+        padding: EdgeInsets.zero,
+        children: [
+          Column(
+            children: [
+              SizedBox(
+                height: 10,
+              ),
+              buildDragHandle(),
+              SizedBox(
+                height: 25,
+              ),
+              Text("We found n drivers for your request "),
+              SizedBox(
+                height: 20,
+              ),
+              Container(
+                color: Colors.green,
+                width: double.infinity,
+                height: 330,
+                child: ListView(
+                  scrollDirection: Axis.horizontal,
+                  padding: EdgeInsets.all(20),
+                  children: [
+                    Container(
+                      width: 240,
+                      color: Colors.teal.shade100,
+                      margin: const EdgeInsets.all(10),
+                      child: Column(
+                        children: [
+                          const SizedBox(height: 10),
+                          const CircleAvatar(
+                            radius: 40,
+                            backgroundColor: Colors.blue,
+                          ),
+                          const SizedBox(height: 10),
+                          const Text('Paolo Pepito'),
+                          const SizedBox(height: 70),
+                          Container(
+                            height: 35,
+                            width: 180,
+                            decoration: const BoxDecoration(
+                              color: Colors.blue,
+                              borderRadius:
+                                  BorderRadius.all(Radius.circular(5)),
+                            ),
+                            child: const Center(child: Text("Hail ride")),
+                          )
+                        ],
+                      ),
+                    ),
+                    Container(
+                      width: 240,
+                      color: Colors.teal.shade100,
+                      margin: const EdgeInsets.all(10),
+                      child: Column(
+                        children: [
+                          const SizedBox(height: 10),
+                          const CircleAvatar(
+                            radius: 40,
+                            backgroundColor: Colors.blue,
+                          ),
+                          const SizedBox(height: 10),
+                          const Text('Charles Ausejo'),
+                          const SizedBox(height: 70),
+                          Container(
+                            height: 35,
+                            width: 180,
+                            decoration: const BoxDecoration(
+                              color: Colors.blue,
+                              borderRadius:
+                                  BorderRadius.all(Radius.circular(5)),
+                            ),
+                            child: const Center(child: Text("Hail ride")),
+                          )
+                        ],
+                      ),
+                    ),
+                    Container(
+                      width: 240,
+                      color: Colors.teal.shade100,
+                      margin: const EdgeInsets.all(10),
+                      child: Column(
+                        children: [
+                          const SizedBox(height: 10),
+                          const CircleAvatar(
+                            radius: 40,
+                            backgroundColor: Colors.blue,
+                          ),
+                          const SizedBox(height: 10),
+                          const Text('Blithe Gonzales'),
+                          const SizedBox(height: 70),
+                          Container(
+                            height: 35,
+                            width: 180,
+                            decoration: const BoxDecoration(
+                              color: Colors.blue,
+                              borderRadius:
+                                  BorderRadius.all(Radius.circular(5)),
+                            ),
+                            child: const Center(child: Text("Hail ride")),
+                          )
+                        ],
+                      ),
+                    ),
+                  ],
+                ),
+              )
+            ],
+          )
+        ],
+      );
+}
+
+Widget buildDragHandle() => Container(
+      width: 40,
+      height: 5,
+      decoration: BoxDecoration(
+          color: Colors.grey[300], borderRadius: BorderRadius.circular(12)),
+    );
