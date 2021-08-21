4a0420996f80072fdc31840a144059c3f3fe7894
Paolo Pepito <npaolopepito@gmail.com>
2021-08-21 09:18:35 +0800

    Remove StopPointsViewState as it is not needed yet

--- lib/logic/driver/stop_points/stop_points_view_state.dart
+++
@@ -1,5 +0,0 @@
-import 'package:google_maps_flutter/google_maps_flutter.dart';
-import 'package:hooks_riverpod/hooks_riverpod.dart';
-
-final cameraPositionProvider =
-    StateProvider.autoDispose<CameraPosition?>((ref) => null);
