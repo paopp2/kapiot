30d0271199b3e98b61d72e22083370f1a1734a10
Paolo Pepito <npaolopepito@gmail.com>
2021-08-21 11:39:41 +0800

    Add 'const' keyword or constant constructors

--- lib/ui/driver/rider_manager_view.dart
+++ lib/ui/driver/rider_manager_view.dart
@@ -12,42 +12,41 @@ class RiderManagerView extends HookConsumerWidget {
       builder: (context, constraints) {
         return Scaffold(
           body: Container(
-            color: Color(0x7679ADFf),
+            color: const Color(0x7679ADFf),
             child: Padding(
-              padding: EdgeInsets.all(15.0),
+              padding: const EdgeInsets.all(15.0),
               child: Column(
                 children: [
-                  SizedBox(height: 50.0),
-                  Text("Next Stop"),
+                  const SizedBox(height: 50.0),
+                  const Text("Next Stop"),
                   Expanded(
                       flex: 2,
                       child: ListView.builder(
                           itemCount: model.acceptedRidersStopPoints.length,
                           itemBuilder: (context, index) {
-                            return Container(
-                                child: Padding(
-                              padding: EdgeInsets.all(10.0),
+                            return Padding(
+                              padding: const EdgeInsets.all(10.0),
                               child: Row(
                                 children: [
-                                  CircleAvatar(
+                                  const CircleAvatar(
                                     backgroundImage: NetworkImage(
                                         'https://www.passerellesnumeriques.org/wp-content/uploads/2016/09/USC.png'),
                                   ),
                                   Padding(
-                                    padding: EdgeInsets.only(
+                                    padding: const EdgeInsets.only(
                                         left: 20.0, right: 20.0),
                                     child: Text(
                                         model.acceptedRidersStopPoints[index]
                                             .toString(),
-                                        style: TextStyle(fontSize: 13.0)),
+                                        style: const TextStyle(fontSize: 13.0)),
                                   ),
                                   ElevatedButton(
                                     onPressed: () {},
-                                    child: Text('Accept'),
+                                    child: const Text('Accept'),
                                   ),
                                 ],
                               ),
-                            ));
+                            );
                           })),
                   Expanded(
                     flex: 4,
@@ -57,28 +56,27 @@ class RiderManagerView extends HookConsumerWidget {
                           shrinkWrap: true,
                           itemCount: model.requestingRiders.length,
                           itemBuilder: (context, index) {
-                            return Container(
-                                child: Padding(
-                              padding: EdgeInsets.all(10.0),
+                            return Padding(
+                              padding: const EdgeInsets.all(10.0),
                               child: Row(
                                 children: [
-                                  CircleAvatar(
+                                  const CircleAvatar(
                                     backgroundImage: NetworkImage(
                                         'https://www.passerellesnumeriques.org/wp-content/uploads/2016/09/USC.png'),
                                   ),
                                   Padding(
-                                    padding: EdgeInsets.only(
+                                    padding: const EdgeInsets.only(
                                         left: 20.0, right: 20.0),
                                     child: Text(model.requestingRiders[index],
-                                        style: TextStyle(fontSize: 20.0)),
+                                        style: const TextStyle(fontSize: 20.0)),
                                   ),
                                   ElevatedButton(
                                     onPressed: () {},
-                                    child: Text('Accept'),
+                                    child: const Text('Accept'),
                                   ),
                                 ],
                               ),
-                            ));
+                            );
                           }),
                     ),
                   ),
