120a412fa65883ddd631d97c438238381689e667
Paolo Pepito <npaolopepito@gmail.com>
2021-08-22 23:59:17 +0800

    Remove cameraPositionProvider

--- lib/logic/home/home_view_state.dart
+++ lib/logic/home/home_view_state.dart
@@ -2,9 +2,6 @@ import 'package:google_maps_flutter/google_maps_flutter.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
 import 'package:kapiot/model/kapiot_location/kapiot_location.dart';
 
-final cameraPositionProvider =
-    StateProvider.autoDispose<CameraPosition?>((ref) => null);
-
 final isRiderSelectedProvider = StateProvider.autoDispose<bool>((ref) => true);
 
 final riderCountProvider = StateProvider.autoDispose<int>((ref) => 1);
