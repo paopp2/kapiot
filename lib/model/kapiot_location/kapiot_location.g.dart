b9b6111f7b392d327841f5cd9a77fcaba714e23b
Paolo Pepito <npaolopepito@gmail.com>
2021-08-22 21:37:43 +0800

    Add optional 'address' field for KapiotLocation model

--- lib/model/kapiot_location/kapiot_location.g.dart
+++ lib/model/kapiot_location/kapiot_location.g.dart
@@ -10,10 +10,12 @@ _$_KapiotLocation _$$_KapiotLocationFromJson(Map<String, dynamic> json) =>
     _$_KapiotLocation(
       lat: (json['lat'] as num).toDouble(),
       lng: (json['lng'] as num).toDouble(),
+      address: json['address'] as String?,
     );
 
 Map<String, dynamic> _$$_KapiotLocationToJson(_$_KapiotLocation instance) =>
     <String, dynamic>{
       'lat': instance.lat,
       'lng': instance.lng,
+      'address': instance.address,
     };
