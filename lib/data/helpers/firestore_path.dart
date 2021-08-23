4927a38e7ac24d5c7a267c4fa2e6912b85eedfe7
Paolo Pepito <npaolopepito@gmail.com>
2021-08-23 19:28:37 +0800

    Implement initial repositories

--- lib/data/helpers/firestore_path.dart
+++ lib/data/helpers/firestore_path.dart
@@ -1,1 +1,19 @@
-class FirestorePath {}
+import 'dart:core';
+
+class FirestorePath {
+  // COLLECTION paths
+  static String colActiveDrivers() => 'active_drivers';
+  static String colActiveRiders() => 'active_riders';
+  static String colRequestingRiders(String driverId) =>
+      'active_drivers/$driverId/requests';
+  static String colAcceptedRiders(String driverId) =>
+      'active_drivers/$driverId/accepted';
+
+  // DOCUMENT paths
+  static String docActiveDriver(String driverId) => 'active_drivers/$driverId';
+  static String docActiveRider(String riderId) => 'active_rider/$riderId';
+  static String docActiveDriverRequest(String driverId, String riderId) =>
+      'active_drivers/$driverId/requests/$riderId';
+  static String docActiveDriverAccepted(String driverId, String riderId) =>
+      'active_drivers/$driverId/accepted/$riderId';
+}
--- lib/data/repositories/rider_repository.dart
+++ lib/data/repositories/rider_repository.dart
@@ -1,1 +1,32 @@
-class RiderRepository {}
+import 'package:hooks_riverpod/hooks_riverpod.dart';
+import 'package:kapiot/data/helpers/firestore_helper.dart';
+import 'package:kapiot/data/helpers/firestore_path.dart';
+import 'package:kapiot/model/kapiot_user/kapiot_user.dart';
+import 'package:kapiot/model/route_config/route_config.dart';
+
+final riderRepositoryProvider = Provider.autoDispose(
+  (ref) => RiderRepository(firestoreHelper: FirestoreHelper.instance),
+);
+
+class RiderRepository {
+  RiderRepository({required this.firestoreHelper});
+  final FirestoreHelper firestoreHelper;
+
+  void pushRiderConfig(RouteConfig routeConfig) async {
+    assert(routeConfig is ForRider);
+    await firestoreHelper.setData(
+      path: FirestorePath.colActiveRiders(),
+      data: routeConfig.toJson(),
+    );
+  }
+
+  // TODO: Kapiot algorithm here
+  Stream<List<KapiotUser>> getCompatibleDrivers(KapiotUser rider) {
+    final compatibleDriversStream = firestoreHelper.collectionStream(
+      path: FirestorePath.colActiveDrivers(),
+      builder: (data, docID) => RouteConfig.fromJson(data),
+    );
+    return compatibleDriversStream
+        .map((routeConfig) => routeConfig.map((rc) => rc.user).toList());
+  }
+}
