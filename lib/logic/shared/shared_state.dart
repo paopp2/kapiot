d3a85621c9bb0b3587cd18c97ce256e61a5a2b4c
Charles Jr Ausejo <charlesjrausejo@gmail.com>
2021-08-24 14:24:51 +0800

    Modify currentRouteConfigProvider to not have autoDispose

--- lib/logic/shared/shared_state.dart
+++ lib/logic/shared/shared_state.dart
@@ -1,5 +1,4 @@
 import 'package:hooks_riverpod/hooks_riverpod.dart';
 import 'package:kapiot/model/route_config/route_config.dart';
 
-final currentRouteConfigProvider =
-    StateProvider.autoDispose<RouteConfig?>((ref) => null);
+final currentRouteConfigProvider = StateProvider<RouteConfig?>((ref) => null);
