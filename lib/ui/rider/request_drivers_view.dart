c0cd995e61bbf600fb3fbbf8576685757f5e84e2
Paolo Pepito <npaolopepito@gmail.com>
2021-08-24 15:25:48 +0800

    Listen to compatibleDriversStream in RequestDriversView

--- lib/ui/rider/request_drivers_view.dart
+++ lib/ui/rider/request_drivers_view.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter_hooks/flutter_hooks.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
 import 'package:kapiot/logic/rider/request_drivers_view_model.dart';
+import 'package:kapiot/logic/rider/request_drivers_view_state.dart';
 import 'package:kapiot/ui/shared/kapiot_sliding_panel.dart';
 
 class RequestDriversView extends HookConsumerWidget {
@@ -10,6 +11,7 @@ class RequestDriversView extends HookConsumerWidget {
   @override
   Widget build(BuildContext context, WidgetRef ref) {
     final model = ref.watch(requestDriversViewModel);
+    final compatibleDrivers = ref.watch(compatibleDriversStreamProvider);
 
     useEffect(() {
       model.initState();
@@ -28,33 +30,42 @@ class RequestDriversView extends HookConsumerWidget {
             panel: SizedBox(
               width: double.infinity,
               height: 330,
-              child: ListView.builder(
-                scrollDirection: Axis.horizontal,
-                itemBuilder: (context, index) {
-                  return Container(
-                    width: 240,
-                    color: Colors.grey[100],
-                    margin: const EdgeInsets.all(10),
-                    child: Column(
-                      children: <Widget>[
-                        const SizedBox(height: 10),
-                        const CircleAvatar(
-                          radius: 40,
-                          backgroundColor: Colors.blue,
-                        ),
-                        const SizedBox(height: 10),
-                        const Text('Blithe Gonzales'),
-                        const SizedBox(height: 70),
-                        ElevatedButton(
-                          onPressed: () {
-                            print("hailed");
-                          },
-                          child: const Text('Hail Ride'),
-                        ),
-                      ],
-                    ),
-                  );
-                },
+              child: compatibleDrivers.when(
+                error: (_, __) => const Center(
+                  child: Text('Error'),
+                ),
+                loading: () => const Center(
+                  child: CircularProgressIndicator(),
+                ),
+                data: (compatibleDrivers) => ListView.builder(
+                  scrollDirection: Axis.horizontal,
+                  itemCount: compatibleDrivers.length,
+                  itemBuilder: (context, index) {
+                    final driver = compatibleDrivers[index];
+                    return Container(
+                      width: 240,
+                      color: Colors.grey[100],
+                      margin: const EdgeInsets.all(10),
+                      child: Column(
+                        children: [
+                          const SizedBox(height: 10),
+                          CircleAvatar(
+                            radius: 40,
+                            backgroundColor: Colors.blue,
+                            backgroundImage: NetworkImage(driver.photoUrl!),
+                          ),
+                          const SizedBox(height: 10),
+                          Text(driver.displayName ?? 'No name'),
+                          const SizedBox(height: 70),
+                          ElevatedButton(
+                            onPressed: () => model.requestDriver(driver.id),
+                            child: const Text('Hail Ride'),
+                          ),
+                        ],
+                      ),
+                    );
+                  },
+                ),
               ),
             ),
           ),
