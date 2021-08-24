94c1273d2e7b3e9471c13123761291021d53cc40
Charles Jr Ausejo <charlesjrausejo@gmail.com>
2021-08-24 11:12:58 +0800

    Add providers in RequestAcceptedViewModel

--- lib/logic/rider/request_accepted_view_model.dart
+++ lib/logic/rider/request_accepted_view_model.dart
@@ -1,1 +1,26 @@
-class RequestAcceptedViewModel {}
+// TODO: Verify provider name
+import 'package:hooks_riverpod/hooks_riverpod.dart';
+import 'package:kapiot/data/core_providers/auth_providers.dart';
+import 'package:kapiot/data/repositories/rider_repository.dart';
+import 'package:kapiot/model/kapiot_user/kapiot_user.dart';
+
+final requestAcceptedViewModelProvider =
+    Provider.autoDispose((ref) => RequestAcceptedViewModel(
+          read: ref.read,
+          riderRepo: ref.watch(riderRepositoryProvider),
+          currentUser: ref.watch(currentUserProvider),
+        ));
+
+class RequestAcceptedViewModel {
+  RequestAcceptedViewModel({
+    required this.read,
+    required this.riderRepo,
+    required this.currentUser,
+  });
+  final Reader read;
+  final RiderRepository riderRepo;
+  final KapiotUser? currentUser;
+
+  void initState() {}
+  void dispose() {}
+}
