0162c8403ef6539201dce07fd738c8c29af378b3
Charles Jr Ausejo <charlesjrausejo@gmail.com>
2021-08-26 09:50:29 +0800

    WIP: Add Map Controller

--- lib/ui/rider/components/request_drivers/request_drivers_map.dart
+++ lib/ui/rider/components/request_drivers/request_drivers_map.dart
@@ -1,7 +1,10 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_hooks/flutter_hooks.dart';
+import 'package:google_maps_flutter/google_maps_flutter.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
+import 'package:kapiot/logic/home/home_view_state.dart';
 import 'package:kapiot/logic/rider/request_drivers_view_model.dart';
+import 'package:kapiot/ui/home/components/loading_screen.dart';
 
 class RequestDriversViewMap extends HookConsumerWidget {
   const RequestDriversViewMap({
@@ -13,6 +16,17 @@ class RequestDriversViewMap extends HookConsumerWidget {
 
   @override
   Widget build(BuildContext context, WidgetRef ref) {
-    return Container();
+    final startLocation = ref.watch(startLocProvider).state;
+    final markers = ref.watch(startEndMarkersProvider).state;
+    final polylines = ref.watch(polylinesProvider).state;
+    return (startLocation == null)
+        ? const LoadingScreen(text: "Fetching current location...")
+        : GoogleMap(
+            mapType: MapType.normal,
+            initialCameraPosition: model.mapController.initialCameraPosition,
+            onMapCreated: model.mapController.onMapCreated,
+            markers: markers,
+            polylines: polylines,
+          );
   }
 }
