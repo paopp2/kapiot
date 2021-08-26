a1041fc63871d2e1265f210bda308bcf1a15e849
Charles Jr Ausejo <charlesjrausejo@gmail.com>
2021-08-26 14:29:26 +0800

    Add necessary providers to display GoogleMap

--- lib/logic/rider/request_accepted_view_model.dart
+++ lib/logic/rider/request_accepted_view_model.dart
@@ -3,23 +3,41 @@ import 'package:kapiot/data/core_providers/auth_providers.dart';
 import 'package:kapiot/data/repositories/rider_repository.dart';
 import 'package:kapiot/logic/shared/shared_state.dart';
 import 'package:kapiot/logic/shared/view_model.dart';
+import 'package:kapiot/data/services/google_maps_api_services.dart';
+import 'package:kapiot/data/services/location_service.dart';
 import 'package:kapiot/model/kapiot_user/kapiot_user.dart';
 
-final requestAcceptedViewModel =
-    Provider.autoDispose((ref) => RequestAcceptedViewModel(
-          read: ref.read,
-          riderRepo: ref.watch(riderRepositoryProvider),
-          currentUser: ref.watch(currentUserProvider),
-        ));
+import 'request_accepted_map_controller.dart';
+
+final requestAcceptedViewModel = Provider.autoDispose(
+  (ref) => RequestAcceptedViewModel(
+    read: ref.read,
+    riderRepo: ref.watch(riderRepositoryProvider),
+    currentUser: ref.watch(currentUserProvider),
+    locationService: ref.watch(locationServiceProvider),
+    googleMapsApiServices: ref.watch(googleMapsApiServicesProvider),
+    mapController: RequestAcceptedMapController(
+      read: ref.read,
+      locationService: ref.watch(locationServiceProvider),
+      googleMapsApiServices: ref.watch(googleMapsApiServicesProvider),
+    ),
+  ),
+);
 
 class RequestAcceptedViewModel extends ViewModel {
   RequestAcceptedViewModel({
     required Reader read,
     required this.riderRepo,
     required this.currentUser,
+    required this.locationService,
+    required this.googleMapsApiServices,
+    required this.mapController,
   }) : super(read);
   final RiderRepository riderRepo;
   final KapiotUser? currentUser;
+  final RequestAcceptedMapController mapController;
+  final LocationService locationService;
+  final GoogleMapsApiServices googleMapsApiServices;
 
   @override
   void initState() {
