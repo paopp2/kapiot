d919dae2b838feff37897813e28e02e5ec73dee3
Charles Jr Ausejo <charlesjrausejo@gmail.com>
2021-08-26 14:32:14 +0800

    Add functions to display and configure map

---
+++ lib/logic/rider/request_accepted_map_controller.dart
@@ -0,0 +1,89 @@
+import 'dart:async';
+import 'package:flutter/material.dart';
+import 'package:google_maps_flutter/google_maps_flutter.dart';
+import 'package:hooks_riverpod/hooks_riverpod.dart';
+import 'package:kapiot/data/services/google_maps_api_services.dart';
+import 'package:kapiot/data/services/location_service.dart';
+import 'package:kapiot/model/kapiot_location/kapiot_location.dart';
+
+const double latLngBoundsPadding = 50.0;
+
+class RequestAcceptedMapController {
+  RequestAcceptedMapController({
+    required this.read,
+    required this.locationService,
+    required this.googleMapsApiServices,
+  });
+
+  Future<void> initializeMap() async {
+    final currentLoc = await locationService.getLocation();
+    initialCameraPosition = CameraPosition(
+      target: LatLng(currentLoc.lat, currentLoc.lng),
+      zoom: 20,
+    );
+    read(startLocProvider).state = currentLoc;
+    final temp = read(startLocProvider).state;
+    print(temp);
+    addMarker(
+      markerId: "start_location",
+      location: currentLoc,
+    );
+  }
+
+  void onMapCreated(GoogleMapController gmapController) async {
+    _controller.complete(gmapController);
+  }
+
+  // TODO: Change starLocProvider to appropriate provider. Currently reading from HomeViewState
+  Future<void> showRoute({
+    required KapiotLocation start,
+    required KapiotLocation end,
+  }) async {
+    GoogleMapController controller = await _controller.future;
+    read(startEndMarkersProvider).state = {};
+    addMarker(markerId: "start_location", location: start);
+    addMarker(markerId: "end_location", location: end);
+    final routeCoordinates = await googleMapsApiServices.directions
+        .getRouteCoordinates(start: start, end: end);
+    read(routeCoordinatesProvider).state = routeCoordinates;
+    drawRouteOutline(routeCoordinates);
+    controller.animateCamera(
+      CameraUpdate.newLatLngBounds(
+        (start.lat <= end.lat)
+            ? LatLngBounds(
+                southwest: LatLng(start.lat, start.lng),
+                northeast: LatLng(end.lat, end.lng),
+              )
+            : LatLngBounds(
+                southwest: LatLng(end.lat, end.lng),
+                northeast: LatLng(start.lat, start.lng),
+              ),
+        latLngBoundsPadding,
+      ),
+    );
+  }
+
+  // TODO: drawRouteOutline and addMarker are Shared functions for all the maps
+  void drawRouteOutline(List<LatLng> routeCoordinates) {
+    final polylines = read(polylinesProvider).state;
+    Polyline polyline = Polyline(
+      polylineId: const PolylineId("poly"),
+      color: Colors.red,
+      points: routeCoordinates,
+    );
+    polylines.add(polyline);
+    read(polylinesProvider).state = polylines;
+  }
+
+  void addMarker({required String markerId, required KapiotLocation location}) {
+    final markers = read(startEndMarkersProvider).state;
+    markers.add(
+      Marker(
+        markerId: MarkerId(markerId),
+        position: LatLng(location.lat, location.lng),
+        icon: BitmapDescriptor.defaultMarker,
+      ),
+    );
+    read(startEndMarkersProvider).state = markers;
+  }
+}
