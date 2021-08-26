017f8c134bf60729133f2f23da86f045d86ed4d4
Paolo Pepito <npaolopepito@gmail.com>
2021-08-26 12:44:59 +0800

    Restore deleted Driver Info section in RequestAcceptedView

--- lib/ui/rider/request_accepted_view.dart
+++ lib/ui/rider/request_accepted_view.dart
@@ -15,66 +15,81 @@ class RequestAcceptedView extends StatelessWidget {
             map: const Center(
               child: Text('Here lies Map'),
             ),
-            panel: Stack(
-              alignment: Alignment.bottomCenter,
+            panel: Column(
               children: [
-                Container(
-                  decoration: BoxDecoration(
-                    borderRadius: const BorderRadius.vertical(
-                      top: Radius.circular(20),
-                    ),
-                    color: Colors.grey.shade200,
-                  ),
-                  height: constraints.maxHeight * 0.28,
-                  child: Column(
-                    children: [
-                      const SizedBox(
-                        height: 10,
+                const CircleAvatar(
+                  radius: 40,
+                  backgroundColor: Colors.blue,
+                ),
+                const SizedBox(height: 10),
+                const Text('Paolo Pepito'),
+                const SizedBox(
+                  height: 10,
+                ),
+                Stack(
+                  alignment: Alignment.bottomCenter,
+                  children: [
+                    Container(
+                      decoration: BoxDecoration(
+                        borderRadius: const BorderRadius.vertical(
+                          top: Radius.circular(20),
+                        ),
+                        color: Colors.grey.shade200,
                       ),
-                      const Center(
-                        child: Text('Your co passengers for this ride'),
-                      ),
-                      const SizedBox(height: 15),
-                      Row(
-                        mainAxisAlignment: MainAxisAlignment.center,
+                      height: constraints.maxHeight * 0.28,
+                      child: Column(
                         children: [
-                          Container(
-                            margin: const EdgeInsets.symmetric(horizontal: 5),
-                            child: const CircleAvatar(
-                              radius: 30,
-                              backgroundColor: Colors.blue,
-                            ),
+                          const SizedBox(
+                            height: 10,
                           ),
-                          Container(
-                            margin: const EdgeInsets.symmetric(horizontal: 5),
-                            child: const CircleAvatar(
-                              radius: 30,
-                              backgroundColor: Colors.blue,
-                            ),
+                          const Center(
+                            child: Text('Your co passengers for this ride'),
+                          ),
+                          const SizedBox(height: 15),
+                          Row(
+                            mainAxisAlignment: MainAxisAlignment.center,
+                            children: [
+                              Container(
+                                margin:
+                                    const EdgeInsets.symmetric(horizontal: 5),
+                                child: const CircleAvatar(
+                                  radius: 30,
+                                  backgroundColor: Colors.blue,
+                                ),
+                              ),
+                              Container(
+                                margin:
+                                    const EdgeInsets.symmetric(horizontal: 5),
+                                child: const CircleAvatar(
+                                  radius: 30,
+                                  backgroundColor: Colors.blue,
+                                ),
+                              ),
+                            ],
                           ),
                         ],
                       ),
-                    ],
-                  ),
-                ),
-                Container(
-                  width: double.infinity,
-                  height: constraints.maxHeight * 0.12,
-                  decoration: const BoxDecoration(
-                    color: Colors.white,
-                    borderRadius: BorderRadius.only(
-                      topLeft: Radius.circular(20),
-                      topRight: Radius.circular(20),
                     ),
-                  ),
-                  child: Column(
-                    children: const [
-                      SizedBox(
-                        height: 10,
+                    Container(
+                      width: double.infinity,
+                      height: constraints.maxHeight * 0.12,
+                      decoration: const BoxDecoration(
+                        color: Colors.white,
+                        borderRadius: BorderRadius.only(
+                          topLeft: Radius.circular(20),
+                          topRight: Radius.circular(20),
+                        ),
                       ),
-                      Text("Rider's reviews"),
-                    ],
-                  ),
+                      child: Column(
+                        children: const [
+                          SizedBox(
+                            height: 10,
+                          ),
+                          Text("Rider's reviews"),
+                        ],
+                      ),
+                    ),
+                  ],
                 ),
               ],
             ),
