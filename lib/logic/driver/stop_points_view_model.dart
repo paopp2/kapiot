f6f0a4d1ce20c5898856b6048ee0bfa4680e505d
Paolo Pepito <npaolopepito@gmail.com>
2021-08-26 10:08:02 +0800

    Refactor existing ViewModels to extend the abstract class ViewModel

--- lib/logic/driver/stop_points_view_model.dart
+++ lib/logic/driver/stop_points_view_model.dart
@@ -2,13 +2,13 @@ import 'dart:async';
 
 import 'package:google_maps_flutter/google_maps_flutter.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
+import 'package:kapiot/logic/shared/view_model.dart';
 
 final stopPointsViewModel = Provider.autoDispose<StopPointsViewModel>(
     (ref) => StopPointsViewModel(ref.read));
 
-class StopPointsViewModel {
-  StopPointsViewModel(this.read);
-  final Reader read;
+class StopPointsViewModel extends ViewModel {
+  StopPointsViewModel(Reader read) : super(read);
   final Completer<GoogleMapController> _controller = Completer();
   final CameraPosition lake = const CameraPosition(
       bearing: 192.8334901395799,
