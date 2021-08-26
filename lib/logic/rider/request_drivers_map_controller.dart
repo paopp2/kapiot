896cc29c43d27f802fbe2c44ba6be9d72b44d7a7
Charles Jr Ausejo <charlesjrausejo@gmail.com>
2021-08-26 10:06:23 +0800

    Modify code to temporarily share the same location from home_view_state

--- lib/logic/rider/request_drivers_map_controller.dart
+++ lib/logic/rider/request_drivers_map_controller.dart
@@ -5,7 +5,7 @@ import 'package:google_maps_flutter/google_maps_flutter.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
 import 'package:kapiot/data/services/google_maps_api_services.dart';
 import 'package:kapiot/data/services/location_service.dart';
-import 'package:kapiot/logic/rider/request_drivers_view_state.dart';
+import 'package:kapiot/logic/home/home_view_state.dart';
 import 'package:kapiot/model/kapiot_location/kapiot_location.dart';
 
 const double latLngBoundsPadding = 50.0;
@@ -39,6 +39,7 @@ class RequestDriversMapController {
     _controller.complete(gmapController);
   }
 
+  // TODO: Change starLocProvider to appropriate provider. Currently reading from HomeViewState
   Future<void> showRoute({
     required KapiotLocation start,
     required KapiotLocation end,
