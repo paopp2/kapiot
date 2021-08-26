04a0540eb45fcd0bcbfb3f37e1a957cc037237fd
Charles Jr Ausejo <charlesjrausejo@gmail.com>
2021-08-26 09:35:20 +0800

    Change in View name and require a View Model

--- lib/ui/rider/components/request_drivers/request_drivers_map.dart
+++ lib/ui/rider/components/request_drivers/request_drivers_map.dart
@@ -1,9 +1,15 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_hooks/flutter_hooks.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
+import 'package:kapiot/logic/rider/request_drivers_view_model.dart';
 
-class RequestDriversMap extends StatelessWidget {
-  const RequestDriversMap({Key? key}) : super(key: key);
+class RequestDriversViewMap extends StatelessWidget {
+  const RequestDriversViewMap({
+    required this.model,
+    Key? key,
+  }) : super(key: key);
+
+  final RequestDriversViewModel model;
 
   @override
   Widget build(BuildContext context) {
