563c685846bb70ec3c179031bce6c8a74e050c5e
Charles Jr Ausejo <charlesjrausejo@gmail.com>
2021-08-26 09:30:21 +0800

    Add RequestDriversMap as a component

---
+++ lib/ui/rider/components/request_drivers/request_drivers_map.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_hooks/flutter_hooks.dart';
+import 'package:hooks_riverpod/hooks_riverpod.dart';
+
+class RequestDriversMap extends StatelessWidget {
+  const RequestDriversMap({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
