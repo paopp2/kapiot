cf3c63719a42b7834df5d116c597abd41df3e4eb
crisxchan <chrisgonzales.online@gmail.com>
2021-08-27 13:10:34 +0800

    Change to Card widget and add onTap method to DriverCard

--- lib/ui/rider/request_drivers/components/driver_card.dart
+++ lib/ui/rider/request_drivers/components/driver_card.dart
@@ -14,26 +14,36 @@ class DriverCard extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container(
+    return SizedBox(
       width: 240,
-      color: Colors.grey[100],
-      margin: const EdgeInsets.all(10),
-      child: Column(
-        children: [
-          const SizedBox(height: 30),
-          CircleAvatar(
-            radius: 40,
-            backgroundColor: Colors.blue,
-            backgroundImage: NetworkImage(driver.photoUrl!),
+      child: Card(
+        color: Colors.grey[100],
+        margin: const EdgeInsets.all(10),
+        child: InkWell(
+          onTap: () {
+            print('card tapped');
+          },
+          splashColor: Colors.black,
+          child: Column(
+            children: [
+              const SizedBox(height: 30),
+              CircleAvatar(
+                radius: 40,
+                backgroundColor: Colors.blue,
+                backgroundImage: NetworkImage(driver.photoUrl!),
+              ),
+              const SizedBox(height: 10),
+              SingleChildScrollView(
+                  scrollDirection: Axis.horizontal,
+                  child: Text(driver.displayName ?? 'No name')),
+              const SizedBox(height: 70),
+              ElevatedButton(
+                onPressed: () => model.requestDriver(driver.id),
+                child: const Text('Hail Ride'),
+              ),
+            ],
           ),
-          const SizedBox(height: 10),
-          Text(driver.displayName ?? 'No name'),
-          const SizedBox(height: 70),
-          ElevatedButton(
-            onPressed: () => model.requestDriver(driver.id),
-            child: const Text('Hail Ride'),
-          ),
-        ],
+        ),
       ),
     );
   }
