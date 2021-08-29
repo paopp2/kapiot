c7e4ac33d843e57f1228ef456b2e6e03b568c56f
Charles Jr Ausejo <charlesjrausejo@gmail.com>
2021-08-29 20:18:59 +0800

    Add data to use in emulator

---
+++ functions/test_data.js
@@ -0,0 +1,41 @@
+/* eslint-disable linebreak-style */
+/* eslint-disable indent */
+const ridersList = [
+    {
+        id: "rider1",
+        name: "John",
+        address: "Mandaue",
+    },
+    {
+        id: "rider2",
+        name: "Bob",
+        address: "Lapu-Lapu",
+    },
+];
+
+const driversList = [
+    {
+        id: "driver1",
+        name: "Earl",
+        address: "Talamban",
+    },
+    {
+        id: "driver2",
+        name: "Bob",
+        address: "Consolacion",
+    },
+];
+
+const singleRider = {
+    id: "single_add",
+    name: "Jane",
+    address: "Tunga sa old bridge",
+};
+
+module.exports = {
+    ridersList,
+    driversList,
+    singleRider,
+};
+
+
