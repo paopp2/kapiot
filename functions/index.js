aeba997652073cf8be430d45d17ad1b7ba8d1abe
Paolo Pepito <npaolopepito@gmail.com>
2021-08-29 12:32:05 +0800

    Add initial Cloud Function to populate test riders for development

--- functions/index.js
+++ functions/index.js
@@ -1,9 +1,27 @@
 const functions = require("firebase-functions");
+const admin = require("firebase-admin");
+admin.initializeApp();
 
-// // Create and Deploy Your First Cloud Functions
-// // https://firebase.google.com/docs/functions/write-firebase-functions
-//
-// exports.helloWorld = functions.https.onRequest((request, response) => {
-//   functions.logger.info("Hello logs!", {structuredData: true});
-//   response.send("Hello from Firebase!");
-// });
+exports.populateActiveRiders = functions.https.onRequest(async (req, res) =>  {
+  const original = req.query.text;
+    ridersList.forEach(addRider);
+    
+    var writeResult = ''
+    async function addRider(rider, index, arr) {
+        writeResult += await admin.firestore().collection('active_riders').doc(rider.id).set(rider);
+    }
+    res.json({result: "Populated 'active_riders'"});
+});
+
+const ridersList = [
+    {
+        id: "rider1",
+        name: "John",
+        address: "Here"
+    },
+    {
+        id: "rider2",
+        name: "Bob",
+        address: "There"
+    },
+]
