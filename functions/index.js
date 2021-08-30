37ab0f1fc7dcf08ab41d67edf20cf39812ee4693
Charles Jr Ausejo <charlesjrausejo@gmail.com>
2021-08-30 20:24:29 +0800

    Refactor cloud functions

--- functions/index.js
+++ functions/index.js
@@ -8,42 +8,41 @@ const ridersRef = db.collection('active_riders');
 // TODO: make a temporary collection in firestore that will have all the driverId
 var driverIdList = []; 
 exports.getActiveRiders = functions.https.onRequest(async (req, res) =>  {
-    const snapshot = await db.collection('active_riders').get();
+    const snapshot = await ridersRef.get();
     res.send(snapshot.docs.map(doc => doc.data()))
 });
 
 exports.getActiveDrivers = functions.https.onRequest(async (req, res) =>  {
-    const snapshot = await db.collection('active_drivers').get();
+    const snapshot = await driversRef.get();
     res.send(snapshot.docs.map(doc => doc.data()))
 });
 
 exports.populateActiveRiders = functions.https.onRequest(async (req, res) =>  {
-  const original = req.query.text;
+    const original = req.query.text;
     test_data.ridersList.forEach(addRider);
-    
     var writeResult = ''
     async function addRider(rider, index, arr) {
-        writeResult += await db.collection('active_riders').doc(rider.id).set(rider);
+        writeResult += await ridersRef.doc(rider.id).set(rider);
     }
     res.json({result: "Populated 'active_riders'"});
 });
 
 exports.populateActiveDrivers = functions.https.onRequest(async (req, res) =>  {
     const original = req.query.text;
-      test_data.driversList.forEach(addDriver);
-      
-      var writeResult = ''
-      async function addDriver(driver, index, arr) {
-          writeResult += await db.collection('active_drivers').doc(driver.id).set(driver);
-      }
-      res.json({result: "Populated 'active_drivers'"});
-  });
+    test_data.driversList.forEach(addDriver);
+    
+    var writeResult = ''
+    async function addDriver(driver, index, arr) {
+        writeResult += await driversRef.doc(driver.id).set(driver);
+    }
+    res.json({result: "Populated 'active_drivers'"});
+});
 
 exports.requestDriver = functions.https.onRequest(async (req, res) =>  {
-    const requestedDriver = test_data.driversList[1]; // DRIVER
+    const requestedDriver = test_data.driversList[0]; // DRIVER
     const driverId = requestedDriver.id; 
     //const requestingRider = test_data.requestingRider[0]; // RIDER
-    const requestingRider = test_data.singleRider;
+    const requestingRider = test_data.charlesRider; // change to your name
     const riderId = requestingRider.id; 
     await driversRef.doc(driverId).collection('requests').doc(riderId)
     .create(requestingRider)
@@ -53,9 +52,9 @@ exports.requestDriver = functions.https.onRequest(async (req, res) =>  {
 
 exports.acceptRider = functions.https.onRequest(async (req, res) =>  {
     //const requestingRider = test_data.ridersList[0] // RIDER
-    const requestingRider = test_data.singleRider;
+    const requestingRider = test_data.charlesRider; // change to your name
     const riderId = requestingRider.id; 
-    const acceptingDriver = test_data.driversList[1]; // DRIVER
+    const acceptingDriver = test_data.driversList[0]; // DRIVER
     await driversRef.doc(acceptingDriver.id).collection('requests').doc(riderId)
     .delete();
     await ridersRef.doc(requestingRider.id).update({
--- functions/test_data.js
+++ functions/test_data.js
@@ -3,31 +3,7 @@
 /* eslint-disable indent */
 const ridersList = [
     {
-        id: "rider1",
-        acceptingDriver: null,
-        endLocation: {
-            address: "Pitogo, Cebu, Philippines",
-            lat: 10.3786493,
-            lng: 123.97360830000001,
-        },
-        riderCount: 1,
-        runtimeType: "rider",
-        startLocation: {
-            address: "9W2R+GQF, Mandaue City, Cebu, Central Visayas, Philippines",
-            lat: 10.350618,
-            lng: 123.9415964,
-        },
-        timeOfTrip: "2021-08-27T20:02:56.483457",
-        user: {
-            displayName: "Blithe Jan Tristen Gonzales",
-            email: "18102570@usc.edu.ph",
-            id: "rider1",
-            phoneNumber: "09331239087",
-            photoUrl: "https://lh3.googleusercontent.com/a/AATXAJzgbfEHTT7-WV0Sd2LdNZ4squ9aJYtOx_vQpdwE=s96-c",
-        },
-    },
-    {
-        id: "rider2",
+        id: "3WO9ATwspsMwYCRCgdbXnfpp5r83",
         acceptingDriver: null,
         endLocation: {
             address: "Basak, Lapu-Lapu City, Cebu, Philippines",
@@ -41,20 +17,68 @@ const ridersList = [
             lat: 10.3006433,
             lng: 123.9625267,
         },
+        timeOfTrip: "2021-08-27T20:02:56.483457",
+        user: {
+            displayName: "Charles Jr. Ausejo",
+            email: "16100938@usc.edu.ph",
+            id: "3WO9ATwspsMwYCRCgdbXnfpp5r83",
+            phoneNumber: "",
+            photoUrl: "https://lh3.googleusercontent.com/a/AATXAJzgbfEHTT7-WV0Sd2LdNZ4squ9aJYtOx_vQpdwE=s96-c",
+        },
+    },
+    {
+        id: "HV9BcFRIKMYrQOYzd2gStGqErW12",
+        acceptingDriver: null,
+        endLocation: {
+            address: "Basak, Lapu-Lapu City, Cebu, Philippines",
+            lat: 10.291023899999999,
+            lng: 123.961041,
+        },
+        riderCount: 1,
+        runtimeType: "rider",
+        startLocation: {
+            address: "Mapie Junction, Consolacion, Cebu, Central Visayas, Philippines",
+            lat: 10.3823283,
+            lng: 123.9716133,
+        },
         timeOfTrip: "2021-08-29T06:45:22.819077",
         user: {
             displayName: "Nicolas Paolo Pepito",
             email: "16100825@usc.edu.ph",
-            id: "rider2",
-            phoneNumber: "09451230987",
+            id: "HV9BcFRIKMYrQOYzd2gStGqErW12",
+            phoneNumber: "",
             photoUrl: "https://lh3.googleusercontent.com/a-/AOh14GhoQ2pytZ4kx2_MyWLFd1-lAht5b6rtEGvTpj9L=s96-c",
+        },
+    },
+    {
+        id: "TNZyDLzIrLhS4Bklz5yG1rCoDoF2",
+        acceptingDriver: null,
+        endLocation: {
+            address: "SM Seaside City Cebu, Cebu City, Cebu, Philippines",
+            lat: 10.2816952,
+            lng: 123.88144169999998,
+        },
+        riderCount: 1,
+        runtimeType: "rider",
+        startLocation: {
+            address: "1183, Cebu City, Cebu, Central Visayas, Philippines",
+            lat: 10.3676667,
+            lng: 123.9138917,
+        },
+        timeOfTrip: "2021-08-29T06:45:22.819077",
+        user: {
+            displayName: "Christian Benedict Gonzales",
+            email: "18105024@usc.edu.ph",
+            id: "TNZyDLzIrLhS4Bklz5yG1rCoDoF2",
+            phoneNumber: "",
+            photoUrl: "https://lh3.googleusercontent.com/a-/AOh14Gi6RZJXfmElWMUsZUJ0O7zZwQboH7mEH3Hcnxmc=s96-c",
         },
     },
 ];
 
 const driversList = [
     {
-        id: "driver1",
+        id: "3WO9ATwspsMwYCRCgdbXnfpp5r83",
         encodedRoute: "syz}@}krsViAh@FJRM^SRKHIACj@UjBy@dDiBxAq@zDuAdCw@xIcCrAfCT\VRtBpDrAlBfHzD~DxB",
         riderCount: 2,
         runtimeType: "driver",
@@ -63,12 +87,12 @@ const driversList = [
             displayName: "Charles Jr. Ausejo",
             email: "16100938@usc.edu.ph",
             id: "driver1",
-            phoneNumber: "09450987654",
+            phoneNumber: "",
             photoUrl: "https://lh3.googleusercontent.com/a-/AOh14Gjh4llC1Ytss067CwoSHmRmHaY31OblZIjSdywz=s96-c",
         },
     },
     {
-        id: "driver2",
+        id: "HV9BcFRIKMYrQOYzd2gStGqErW12",
         encodedRoute: "wxj~@setsVCJKPRVjAdBnAlBh@~@^z@L^j@pCp@xDf@|AdCtGTj@x@`CX~@gAj@a@V",
         riderCount: 3,
         runtimeType: "driver",
@@ -76,51 +100,26 @@ const driversList = [
         user: {
             displayName: "Nicolas Paolo Pepito",
             email: "16100825@usc.edu.ph",
-            id: "driver2",
-            phoneNumber: "09451230987",
+            id: "HV9BcFRIKMYrQOYzd2gStGqErW12",
+            phoneNumber: "",
             photoUrl: "https://lh3.googleusercontent.com/a-/AOh14GhoQ2pytZ4kx2_MyWLFd1-lAht5b6rtEGvTpj9L=s96-c",
         },
     },
     {
-        id: "driver3",
+        id: "TNZyDLzIrLhS4Bklz5yG1rCoDoF2",
         encodedRoute: "w|g~@{|hsVCQ@MTLl@Lx@@F_@BIFEdCg@nA_@t@a@VUFMFc@Ka@Ue@AOCm@Ke@i@aA}@yBc@_A_@i@o@m@YPK@ME",
         riderCount: 1,
         runtimeType: "driver",
         timeOfTrip: "2021-08-30T13:45:35.234433",
         user: {
             displayName: "Christian Benedict Gonzales",
-            email: "cgonz@gmail.com",
-            id: "driver3",
-            phoneNumber: "09451231234",
+            email: "18105024@gmail.com",
+            id: "TNZyDLzIrLhS4Bklz5yG1rCoDoF2",
+            phoneNumber: "",
             photoUrl: "https://lh3.googleusercontent.com/a-/AOh14Gi6RZJXfmElWMUsZUJ0O7zZwQboH7mEH3Hcnxmc=s96-c",
         },
     },
 ];
-
-const singleRider = {
-    id: "you",
-    acceptingDriver: null,
-    endLocation: {
-        address: "Pitogo, Cebu, Philippines",
-        lat: 10.3786493,
-        lng: 123.97360830000001,
-    },
-    riderCount: 1,
-    runtimeType: "rider",
-    startLocation: {
-        address: "9W2R+GQF, Mandaue City, Cebu, Central Visayas, Philippines",
-        lat: 10.350618,
-        lng: 123.9415964,
-    },
-    timeOfTrip: "2021-08-27T20:02:56.483457",
-    user: {
-        displayName: "This is You",
-        email: "69696969@usc.edu.ph",
-        id: "you",
-        phoneNumber: "09331239087",
-        photoUrl: "https://lh3.googleusercontent.com/a/AATXAJzgbfEHTT7-WV0Sd2LdNZ4squ9aJYtOx_vQpdwE=s96-c",
-    },
-};
 
 const requestingRider = [
     {
@@ -176,7 +175,6 @@ const requestingRider = [
 module.exports = {
     ridersList,
     driversList,
-    singleRider,
     requestingRider,
 };
 
