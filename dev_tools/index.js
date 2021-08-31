237416b315d4973cd598d1dc77bb75bb7ec7a994
Charles Jr Ausejo <charlesjrausejo@gmail.com>
2021-08-31 10:17:39 +0800

    Add rider and driver ID

--- dev_tools/index.js
+++ dev_tools/index.js
@@ -1,33 +1,28 @@
-function populateAll(e){
-    const wnd = window.open("http://localhost:5001/kapiot-46cbc/us-central1/populateAll");
-    wnd.close();
-    e.preventDefault();
-}
-function acceptRider(e){
-    const rider_index = document.getElementById("rider_index").value.toString();
-    const driver_index = document.getElementById("driver_index").value.toString();
-    const url = "http://localhost:5001/kapiot-46cbc/us-central1/acceptRider?r=" + rider_index + "&d=" + driver_index;
-    const wnd = window.open(url);
-    wnd.close();
-    e.preventDefault();
-}
 function action(e,option){
     var wnd = '';
+    var url = ''
     if(option==4){
-        wnd = window.open("http://localhost:5001/kapiot-46cbc/us-central1/populateAll")
+        url = "http://localhost:5001/kapiot-46cbc/us-central1/populateAll";
     }
-    else if(option==2 || option==3){
+    else if(option==1 || option==2 || option==3){
         const rider_index = document.getElementById("rider_index").value.toString();
         const driver_index = document.getElementById("driver_index").value.toString();
-        if(option==2){
-            window.location = "http://localhost:5001/kapiot-46cbc/us-central1/requestDriver?r=" + rider_index + "&d=" + driver_index;
+        switch(option){
+            case 1:
+                url  = "http://localhost:5001/kapiot-46cbc/us-central1/requestDriver?r=" + rider_index + "&d=" + driver_index;
+                break;
+            case 2:
+                url = "http://localhost:5001/kapiot-46cbc/us-central1/requestAllDrivers?r=" + rider_index;
+                break;
+            case 3: 
+                url = "http://localhost:5001/kapiot-46cbc/us-central1/acceptRider?r=" + rider_index + "&d=" + driver_index;
+                break;
         }
-        else{
-            const url = "http://localhost:5001/kapiot-46cbc/us-central1/acceptRider?r=" + rider_index + "&d=" + driver_index;
-        }
-        
-        wnd = window.open(url);
     }
+    else{
+        alert('An error occured');
+    }
+    wnd = window.open(url);
     wnd.close();
     e.preventDefault();
 }
