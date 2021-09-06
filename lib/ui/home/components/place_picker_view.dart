7e84c045ea873dee4e9485c9e31de8561ddebefd
Paolo Pepito <npaolopepito@gmail.com>
2021-09-06 15:52:29 +0800

    Convert PlacePickerView to HookConsumerWidget

--- lib/ui/home/components/place_picker_view.dart
+++ lib/ui/home/components/place_picker_view.dart
@@ -1,13 +1,14 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
+import 'package:hooks_riverpod/hooks_riverpod.dart';
 import 'package:kapiot/app_router.dart';
 
-class PlacePickerView extends StatelessWidget {
+class PlacePickerView extends HookConsumerWidget {
   const PlacePickerView({Key? key}) : super(key: key);
 
   @override
-  Widget build(BuildContext context) {
+  Widget build(BuildContext context, WidgetRef ref) {
     return LayoutBuilder(
       builder: (BuildContext context, BoxConstraints constraints) {
         return SafeArea(
