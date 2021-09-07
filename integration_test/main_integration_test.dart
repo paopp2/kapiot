df744d54e90414a4808e5f9c33739c5cd669ec79
Paolo Pepito <npaolopepito@gmail.com>
2021-09-08 00:17:54 +0800

    Add a main_integration_test.dart file to collate all integration tests

---
+++ integration_test/main_integration_test.dart
@@ -0,0 +1,20 @@
+import 'dart:io';
+
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'package:integration_test/integration_test.dart';
+
+import 'rider_flow_test.dart' as rider_flow_test;
+
+Future<void> main() async {
+  IntegrationTestWidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
+  final localHostString = Platform.isAndroid ? '10.0.2.2' : 'localhost';
+  FirebaseFirestore.instance.settings = Settings(
+    host: '$localHostString:8080',
+    sslEnabled: false,
+    persistenceEnabled: false,
+  );
+
+  rider_flow_test.main();
+}
