2a2c98b7cb94ac2b8bae33e13a88f5db5a071f4b
Paolo Pepito <npaolopepito@gmail.com>
2021-09-08 00:05:31 +0800

    Update CloudFunctionsApi

--- integration_test/cloud_functions_api.dart
+++ integration_test/cloud_functions_api.dart
@@ -7,9 +7,21 @@ class CloudFunctionsApi {
     await http.get(url);
   }
 
-  static Future<void> acceptRider(int r) async {
+  static Future<void> acceptRider({
+    required int riderIdx,
+    required int driverIdx,
+  }) async {
     final url = Uri.parse(
-        "http://10.0.2.2:5001/kapiot-46cbc/us-central1/acceptRider?r=$r&d=4");
+        "http://10.0.2.2:5001/kapiot-46cbc/us-central1/acceptRider?r=$riderIdx&d=$driverIdx");
+    await http.get(url);
+  }
+
+  static Future<void> dropRider({
+    required int riderIdx,
+    required int driverIdx,
+  }) async {
+    final url = Uri.parse(
+        "http://10.0.2.2:5001/kapiot-46cbc/us-central1/dropRider?r=$riderIdx&d=$driverIdx");
     await http.get(url);
   }
 }
