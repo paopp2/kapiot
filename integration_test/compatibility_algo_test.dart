fd8c5b059c88cca93dfa5069b7fc64c26e90650b
Paolo Pepito <npaolopepito@gmail.com>
2021-09-08 09:53:32 +0800

    Add initial compatibility tests

---
+++ integration_test/compatibility_algo_test.dart
@@ -0,0 +1,171 @@
+import 'package:firebase_auth_mocks/firebase_auth_mocks.dart';
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_test/flutter_test.dart';
+import 'package:hooks_riverpod/hooks_riverpod.dart';
+import 'package:kapiot/app_router.dart';
+import 'package:kapiot/data/core_providers/firebase_providers.dart';
+import 'package:kapiot/main.dart';
+import 'package:kapiot/ui/home/home_view.dart';
+import 'package:kapiot/ui/rider/request_drivers/components/driver_card.dart';
+import 'package:kapiot/ui/rider/request_drivers/request_drivers_view.dart';
+
+import 'cloud_functions_api.dart';
+
+Future<void> main() async {
+  testWidgets(
+    "The correct compatible drivers are shown",
+    (WidgetTester tester) async {
+      final user = MockUser(
+        isAnonymous: false,
+        uid: 'LPRde2OpOF101T42uHSWAc6tvDci',
+        email: 'titan_gel_4200@email.com',
+        displayName: "Titan",
+      );
+      await tester.pumpWidget(ProviderScope(
+        overrides: [
+          fireauthProvider.overrideWithValue(
+            MockFirebaseAuth(signedIn: true, mockUser: user),
+          ),
+        ],
+        child: const MyApp(),
+      ));
+
+      // Populate Firestore data
+      CloudFunctionsApi.populateAll();
+
+      // Wait for loading animation to finish
+      await tester.pumpAndSettle();
+
+      await verifyCompatibility(
+        tester: tester,
+        startAddress: "Prime Care Cebu @ APM CENTRALE",
+        endAddress: "Kaffee Alde Mabolo",
+        expectedDrivers: ["Skeleton Jack"],
+      );
+
+      await verifyCompatibility(
+        tester: tester,
+        startAddress: "AA BBQ, Salinas Drive, Cebu City, Cebu, Philippines",
+        endAddress:
+            "University of Cebu - Banilad Campus, Gov. M. Cuenco Avenue, Cebu City, Cebu, Philippines",
+        expectedDrivers: ["Rick Sanchez"],
+      );
+    },
+  );
+}
+
+Future<void> verifyCompatibility({
+  required WidgetTester tester,
+  required String startAddress,
+  required String endAddress,
+  required List<String> expectedDrivers,
+}) async {
+  final startAddrSplit = startAddress.split(",");
+
+  // String manipulation. Splitting the complete addresses into an input to
+  // type into the place search TextFields, the partial address to search for in
+  // the suggested places and the complete address to check for when it is set
+  // after tapping on them
+  String startAddrInput, startAddrSuggestion;
+  if (startAddrSplit.length > 2) {
+    final partialAddr = "${startAddrSplit[0]}, ${startAddrSplit[1]}";
+    startAddrInput = partialAddr.toLowerCase();
+    startAddrSuggestion = startAddrSplit.first;
+  } else {
+    startAddrInput = startAddrSplit.first.toLowerCase() + " ";
+    startAddrSuggestion = startAddrSplit.first;
+  }
+
+  final endAddrSplit = endAddress.split(",");
+  String endAddrInput, endAddrSuggestion;
+  if (endAddrSplit.length > 2) {
+    final partialAddr = "${endAddrSplit[0]}, ${endAddrSplit[1]}";
+    endAddrInput = partialAddr.toLowerCase();
+    endAddrSuggestion = endAddrSplit.first;
+  } else {
+    endAddrInput = endAddrSplit.first.toLowerCase() + " ";
+    endAddrSuggestion = endAddrSplit.first;
+  }
+
+  // Tapping on the startLoc TextField @HomeView opens the PlacePickerView
+  final startLocTextFieldHome =
+      find.widgetWithText(TextField, "Start location");
+  await tester.tap(startLocTextFieldHome);
+  await tester.pumpAndSettle();
+
+  // StartLoc TextField should be highlighted when PlacePickerView is pumped.
+  // Entering text shows place suggestions. Tapping on the correct ListTile
+  // should set the address to the TextField
+  final startLocTextFieldPlacePicker =
+      find.widgetWithIcon(TextField, CupertinoIcons.smallcircle_circle);
+  await tester.tap(startLocTextFieldPlacePicker);
+  await tester.pumpAndSettle();
+  tester.testTextInput.enterText(startAddrInput);
+  await tester.pumpAndSettle();
+  final correctStartSuggestion =
+      find.widgetWithText(ListTile, startAddrSuggestion);
+  while (findsNothing.matches(correctStartSuggestion, {})) {
+    await tester.pumpAndSettle();
+  }
+  await tester.tap(correctStartSuggestion);
+  final startLocAddress = find.ancestor(
+    of: find.textContaining(startAddress),
+    matching: find.byType(TextField),
+  );
+  while (findsNothing.matches(startLocAddress, {})) {
+    await tester.pumpAndSettle();
+  }
+  expect(startLocAddress, findsOneWidget);
+
+  // Tapping then typing on endLocTextField should show place suggestions.
+  // Pressing on a suggestion should return the app to the HomeView as the
+  // app should return once both locations are set
+  final endLocTextFieldPlacePicker =
+      find.widgetWithIcon(TextField, CupertinoIcons.location);
+  await tester.tap(endLocTextFieldPlacePicker);
+  await tester.pumpAndSettle();
+  tester.testTextInput.enterText(endAddrInput);
+  await tester.pumpAndSettle();
+  final correctEndSuggestion = find.text(endAddrSuggestion);
+  while (findsNothing.matches(correctEndSuggestion, {})) {
+    await tester.pumpAndSettle();
+  }
+  await tester.tap(correctEndSuggestion.first);
+  // final endLocAddress = find.text(endAddrComplete);
+  final endLocAddress = find.ancestor(
+    of: find.textContaining(endAddress),
+    matching: find.byType(TextField),
+  );
+  while (findsNothing.matches(endLocAddress, {})) {
+    await tester.pumpAndSettle();
+  }
+  expect(find.byType(HomeView), findsOneWidget);
+  expect(startLocAddress, findsOneWidget);
+  expect(endLocAddress, findsOneWidget);
+
+  // Rider ChoiceChip should already be selected by default therefore
+  // pressing on next should navigate the app to the RequestDriversView
+  final nextButton = find.widgetWithText(ElevatedButton, "Next");
+  await tester.tap(nextButton);
+  await tester.pumpAndSettle();
+  expect(find.byType(RequestDriversView), findsOneWidget);
+
+  // @RequestDriversView
+  // At RequestDriversView, verify that there are the correct number of shown
+  // (compatible) drivers
+  await tester.pumpAndSettle();
+  expect(find.byType(DriverCard), findsNWidgets(expectedDrivers.length));
+
+  // Tapping on each DriverCard should animate their route on the map
+  for (var driver in expectedDrivers) {
+    var driverCard = find.widgetWithText(DriverCard, driver);
+    await tester.tap(driverCard);
+    await tester.pumpAndSettle();
+  }
+  AppRouter.instance.popView();
+  while (findsNothing.matches(find.byType(HomeView), {})) {
+    await tester.pumpAndSettle();
+  }
+  expect(find.byType(HomeView), findsOneWidget);
+}
