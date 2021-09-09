b4681309ae4617036f2d72def1e364c4a4f1c60b
crisxchan <chrisgonzales.online@gmail.com>
2021-09-09 23:49:27 +0800

    WIP: Update AppBar UI

--- lib/ui/home/home_view.dart
+++ lib/ui/home/home_view.dart
@@ -15,7 +15,6 @@ class HomeView extends HookConsumerWidget {
   @override
   Widget build(context, ref) {
     final model = ref.watch(homeViewModel);
-    final currentUser = ref.watch(currentUserProvider);
     final resetKey = ref.watch(resetKeyProvider).state;
 
     useEffect(() {
@@ -29,34 +28,40 @@ class HomeView extends HookConsumerWidget {
           return Scaffold(
             extendBodyBehindAppBar: true,
             appBar: AppBar(
-              leading: Container(
-                margin: const EdgeInsets.only(top: 10, left: 10),
-                child: CircleAvatar(
-                  radius: 5,
-                  backgroundColor: const Color(0xffffc901),
-                  backgroundImage: NetworkImage(currentUser?.photoUrl ?? ''),
-                ),
-              ),
-              backgroundColor: Colors.transparent,
-              elevation: 0,
-              shape: const RoundedRectangleBorder(
-                borderRadius: BorderRadius.vertical(
-                  bottom: Radius.circular(24),
+              toolbarHeight: constraints.maxHeight * 0.08,
+              centerTitle: true,
+              title: const Text(
+                'kapiot',
+                textAlign: TextAlign.center,
+                style: TextStyle(
+                  fontSize: 42,
+                  fontFamily: 'Sanz',
+                  letterSpacing: 3,
+                  fontWeight: FontWeight.bold,
+                  color: Color(0xffffc901),
+                  // Color(0xff808080),
                 ),
               ),
               actions: [
                 Container(
-                  margin: const EdgeInsets.only(top: 10, right: 10),
-                  decoration: BoxDecoration(
-                    color: const Color(0x5f808080),
-                    borderRadius: BorderRadius.circular(24),
+                  width: 50,
+                  margin: EdgeInsets.only(
+                    right: constraints.maxWidth * 0.025,
                   ),
+                  decoration: const BoxDecoration(
+                      color: Color(0x5f808080), shape: BoxShape.circle),
                   child: IconButton(
-                    icon: const Icon(Icons.logout, color: Colors.white),
+                    icon: const Icon(
+                      Icons.logout,
+                      color: Colors.white,
+                      size: 25,
+                    ),
                     onPressed: model.signOut,
                   ),
-                )
+                ),
               ],
+              backgroundColor: Colors.transparent,
+              elevation: 0,
             ),
             body: KapiotSlidingPanel(
               constraints: constraints,
