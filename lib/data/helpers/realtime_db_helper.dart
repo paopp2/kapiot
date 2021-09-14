e967f90d27e69abc56f7499adbf14b3821e11684
Paolo Pepito <npaolopepito@gmail.com>
2021-09-15 02:52:08 +0800

    Implement collectionStream method for realtime_db_helper

--- lib/data/helpers/realtime_db_helper.dart
+++ lib/data/helpers/realtime_db_helper.dart
@@ -30,4 +30,38 @@ class RealtimeDbHelper {
     );
     return snapshots.map((snapshot) => builder(snapshot));
   }
+
+  Stream<List<T>> collectionStream<T>({
+    required String path,
+    required T Function(Map<String, dynamic> data, String documentID) builder,
+    List<String>? keysOfDocsToGet,
+  }) async* {
+    // Filter stream (sometimes Firebase RTDB emits null events)
+    final Stream<Event> events =
+        dbRef.child(path).onValue.where((e) => (e.snapshot.value != null));
+    // Remap values at event stream into JSON (Map) data
+    var snapshots =
+        events.map((e) => Map<String, dynamic>.from(e.snapshot.value));
+    // If keys of docs to get is specified, filter snapshots stream to only
+    // return those with keys included in the list
+    if (keysOfDocsToGet != null) {
+      snapshots = snapshots.map((event) {
+        final tempMap = event;
+        tempMap.removeWhere((key, value) => !(keysOfDocsToGet.contains(key)));
+        return tempMap;
+      });
+    }
+    // Convert into a Stream of list of all retrieved 'documents' as JSON,
+    // also passing their docID to the builder callback
+    await for (final snapshot in snapshots) {
+      final List<T> tList = [];
+      snapshot.forEach(
+        (key, value) => tList.add(builder(
+          Map<String, dynamic>.from(value), // JSON document data
+          key, // document ID
+        )),
+      );
+      yield tList;
+    }
+  }
 }
