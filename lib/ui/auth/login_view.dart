056eb573bbd0807024e26f42918bf0bc87af82af
Paolo Pepito <npaolopepito@gmail.com>
2021-09-17 04:08:57 +0800

    Remove unnecessarry comments at LoginView

--- lib/ui/auth/login_view.dart
+++ lib/ui/auth/login_view.dart
@@ -104,14 +104,10 @@ class BackgroundClipper extends CustomClipper<Path> {
   @override
   Path getClip(Size size) {
     var path = Path();
-
     path.moveTo(0, size.height * 0.8);
     path.lineTo(0, size.height);
     path.lineTo(size.width, size.height);
     path.lineTo(size.width, 0);
-
-    // path.lineTo(x, y)
-
     return path;
   }
 
