cbd37c87f8683da10ed5cc716be9de973a9929b4
Paolo Pepito <npaolopepito@gmail.com>
2021-09-23 01:35:27 +0800

    WIP: Refactor ServiceErrorView

--- lib/ui/shared/service_error_view.dart
+++ lib/ui/shared/service_error_view.dart
@@ -11,7 +11,44 @@ class ServiceErrorView extends HookConsumerWidget {
     final internetEnabled = ref.watch(hasNetService).state;
     final locationEnabled = ref.watch(hasLocService).state;
     final hasPermission = ref.watch(hasLocPermission).state;
-    final tempVar = (internetEnabled & locationEnabled & hasPermission);
+
+    String getImageAssetName() {
+      if (!hasPermission && !internetEnabled && !locationEnabled) {
+        return 'assets/images/error/no_resource.png';
+      }
+      // TODO: Change image for 'permission denied' case
+      if (!hasPermission) return 'assets/images/error/no_resource.png';
+      if (!internetEnabled) return 'assets/images/error/no_connection.png';
+      if (!locationEnabled) return 'assets/images/error/no_location.png';
+      return 'assets/images/error/no_resource.png';
+    }
+
+    String getErrorText() {
+      if (!hasPermission && !internetEnabled && !locationEnabled) {
+        return 'welp';
+      }
+      // TODO: Change error text for 'permission denied' case
+      if (!hasPermission) return 'welp.';
+      if (!internetEnabled) return 'no connection';
+      if (!locationEnabled) return 'we lost you';
+      return 'Alrights';
+    }
+
+    String getErrorSubText() {
+      if (!hasPermission && !internetEnabled && !locationEnabled) {
+        return 'No internet connection. location disabled. location not shared.\nWe wish to help you, but there\'s nothing we really can do for now.';
+      }
+      if (!hasPermission) {
+        return 'Share us your location and we\'ll take you back.';
+      }
+      if (!internetEnabled) {
+        return 'We know how you feel.\n It\'s just that you lost your internet connection.';
+      }
+      if (!locationEnabled) {
+        return 'Enable your GPS and we\'ll find you in no time.';
+      }
+      return 'Alrights';
+    }
 
     return LayoutBuilder(builder: (context, constraints) {
       return WillPopScope(
@@ -26,13 +63,7 @@ class ServiceErrorView extends HookConsumerWidget {
                 mainAxisAlignment: MainAxisAlignment.center,
                 children: [
                   Image.asset(
-                    tempVar
-                        ? 'assets/images/error/no_resource.png'
-                        : !internetEnabled
-                            ? 'assets/images/error/no_connection.png'
-                            : !locationEnabled || !hasPermission
-                                ? 'assets/images/error/no_location.png'
-                                : '',
+                    getImageAssetName(),
                     height: constraints.maxHeight * 0.35,
                     fit: BoxFit.cover,
                   ),
@@ -42,32 +73,20 @@ class ServiceErrorView extends HookConsumerWidget {
                       bottom: constraints.maxHeight * 0.005,
                     ),
                     child: Text(
-                      tempVar
-                          ? 'welp.'
-                          : !internetEnabled
-                              ? 'no connection'
-                              : 'we lost you.',
+                      getErrorText(),
                       style: GoogleFonts.poppins(
                         fontSize: 34,
                         fontWeight: FontWeight.bold,
-                        color: Color(0xff333333),
+                        color: const Color(0xff333333),
                       ),
                     ),
                   ),
                   Text(
-                    tempVar
-                        ? 'no internet connection. location disabled. location not shared.\nWe wish to help you, but there\'s nothing we really can do for now.'
-                        : !internetEnabled
-                            ? 'We know how you feel.\n It\'s just that you lost your internet connection.'
-                            : !locationEnabled
-                                ? 'Enable your GPS and we\'ll find you in no time.'
-                                : !hasPermission
-                                    ? 'Share us your location and we\'ll take you back.'
-                                    : '',
+                    getErrorSubText(),
                     textAlign: TextAlign.center,
                     style: GoogleFonts.montserrat(
                       fontSize: 12,
-                      color: Color(0xff666666),
+                      color: const Color(0xff666666),
                       height: 1.5,
                     ),
                   ),
