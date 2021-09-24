790b0da6ea06f3fe6ef5bada8ba68093a633b6c1
Charles Jr Ausejo <charlesjrausejo@gmail.com>
2021-09-24 21:55:24 +0800

    WIP: Get list of riderConfigs

--- lib/logic/post_trip/post_trip_summary_view_model.dart
+++ lib/logic/post_trip/post_trip_summary_view_model.dart
@@ -2,6 +2,7 @@ import 'package:hooks_riverpod/hooks_riverpod.dart';
 import 'package:kapiot/data/core/core_providers.dart';
 import 'package:kapiot/data/repositories/rider_repository.dart';
 import 'package:kapiot/logic/driver/rider_manager_view_model.dart';
+import 'package:kapiot/logic/post_trip/post_trip_summary_view_state.dart';
 import 'package:kapiot/logic/shared/shared_state.dart';
 import 'package:kapiot/logic/shared/view_model.dart';
 import 'package:kapiot/model/kapiot_user/kapiot_user.dart';
@@ -30,23 +31,34 @@ class PostTripSummaryViewModel extends ViewModel {
     assert(read(acceptingDriverConfigProvider).state != null);
     assert(read(currentRouteConfigProvider).state != null);
     assert(currentUser != null);
+    getRiderRouteConfigs();
+    final riders = read(ridersRouteConfigs).state;
+    //final riders = getRiderRouteConfigs();
     final acceptingDriver = read(acceptingDriverConfigProvider).state!;
     final currentUserId = currentUser!.id;
-    final transaction = Transaction(currentUserId: currentUserId, driver: acceptingDriver, riders: riders, points: points, startTime: startTime, endTime: endTime, distance: distance)
+    //final transaction = Transaction(currentUserId: currentUserId, driver: acceptingDriver, riders: riders, points: points, startTime: startTime, endTime: endTime, distance: distance)
   }
-  Stream<List<RouteConfig>> getRiderRouteConfigs() {
+
+  Stream<List<RouteConfig>>? getRiderRouteConfigs() {
     final acceptingDriverConfig = read(acceptingDriverConfigProvider).state!;
     final allRidersStream = riderRepo.getRiderConfigsStream(
       driver: acceptingDriverConfig.user,
     );
-    return allRidersStream.map(
-      (riderList) {
-        return riderList
-            .where((rider) => (rider.user.id != currentUser!.id))
-            .toList();
-      },
-    );
+    List<RouteConfig> ridersList = [];
+    if (currentUser is ForDriver) {
+      allRidersStream.forEach((riders) {
+        ridersList.addAll(riders);
+      });
+      read(ridersRouteConfigs).state = ridersList;
+    } else if (currentUser is ForRider) {
+      allRidersStream.forEach((riders) {
+        ridersList
+            .addAll(riders.where((rider) => rider.user.id != currentUser!.id));
+      });
+      read(ridersRouteConfigs).state = ridersList;
+    }
   }
+
   @override
   void dispose() {}
 }
