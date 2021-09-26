b7f87c69f29f944004fa388a7155489b2e1243cd
Paolo Pepito <npaolopepito@gmail.com>
2021-09-27 03:20:05 +0800

    Set Transaction to basic UI at PostTripSummaryView

--- lib/ui/shared/post_trip_summary_view.dart
+++ lib/ui/shared/post_trip_summary_view.dart
@@ -2,17 +2,21 @@ import 'package:flutter/material.dart';
 import 'package:flutter_hooks/flutter_hooks.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
 import 'package:kapiot/logic/post_trip/post_trip_summary_view_model.dart';
+import 'package:kapiot/logic/shared/shared_state.dart';
 
 class PostTripSummaryView extends HookConsumerWidget {
   const PostTripSummaryView({Key? key}) : super(key: key);
 
   @override
-  Widget build(BuildContext context, ref) {
+  Widget build(BuildContext context, WidgetRef ref) {
     final model = ref.watch(postTripSummaryViewModel);
+    final transaction = ref.watch(transactionProvider).state;
+
     useEffect(() {
       model.initState();
       return model.dispose;
     });
+
     return LayoutBuilder(
       builder: (context, constraints) {
         return Scaffold(
@@ -20,15 +24,21 @@ class PostTripSummaryView extends HookConsumerWidget {
             child: Column(
               mainAxisAlignment: MainAxisAlignment.center,
               children: [
-                const Text('CES: 69'),
-                const Text('Origin: Eskina Japan'),
-                const Text('Destination: Eskina Singapore'),
-                const Text('Duration: 69mins'),
-                const Text('Distance: 69km'),
-                const Text('Driver: '),
-                const Text('Riders: '),
+                Text('CES: ${transaction.points}'),
+                Text('Origin: ${transaction.startLocation?.address}'),
+                Text('Destination: ${transaction.endLocation?.address}'),
+                Text('Start time: ${transaction.startTime}'),
+                Text('End time: ${transaction.endTime}'),
+                Text(
+                  'Duration: ${transaction.endTime!.difference(transaction.startTime!)}',
+                ),
+                Text('Distance: ${transaction.distance} km'),
+                Text('Driver: ${transaction.driver!.user.displayName}'),
+                Text(
+                  'Riders: ${transaction.riders!.map((rc) => rc.user.displayName)}',
+                ),
                 ElevatedButton(
-                  onPressed: () {},
+                  onPressed: model.resetToHomeView,
                   child: const Text('Back to HomeView'),
                 ),
               ],
