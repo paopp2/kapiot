7e18180e89f4da96aa59b8fbf0564c43502c8b1f
Charles Jr Ausejo <charlesjrausejo@gmail.com>
2021-10-05 14:35:51 +0800

    WIP: Modify UserInfo Model

--- lib/model/user_info/user_info.dart
+++ lib/model/user_info/user_info.dart
@@ -1,15 +1,22 @@
+import 'package:flutter/material.dart';
 import 'package:freezed_annotation/freezed_annotation.dart';
 import 'package:flutter/foundation.dart';
+import 'package:hooks_riverpod/hooks_riverpod.dart';
 import 'package:kapiot/model/kapiot_location/kapiot_location.dart';
 
 part 'user_info.freezed.dart';
 
 @freezed
 class UserInfo with _$UserInfo {
+  const UserInfo._();
   const factory UserInfo({
-    double? points,
-    String? course,
-    int? year,
-    List<Map<String, KapiotLocation>> savedLocations,
+    required double points,
+    required String course,
+    required int year,
+    required List<Map<String, KapiotLocation>> savedLocations,
+    required bool _isRegisteredDriver,
+    DriverInfo? driverInfo,
   }) = _UserInfo;
+
+  bool get isRegisteredDriver => _isRegisteredDriver;
 }
