f6947a8eac20e4afa50d83a3e85d019491ce539b
crisxchan <chrisgonzales.online@gmail.com>
2021-10-06 15:10:46 +0800

    Fix overflow on tiles expanded

--- lib/ui/home/components/user_info_drawer.dart
+++ lib/ui/home/components/user_info_drawer.dart
@@ -24,6 +24,7 @@ class UserInfoDrawer extends HookConsumerWidget {
     bool isRegisteredDriver = true;
     return SizedBox(
       width: constraints.maxWidth * 0.85,
+      height: constraints.maxHeight,
       child: Drawer(
         child: Column(
           children: [
@@ -77,148 +78,151 @@ class UserInfoDrawer extends HookConsumerWidget {
             SizedBox(
               width: constraints.maxWidth * 0.85,
               height: constraints.maxHeight * 0.7,
-              child: Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: [
-                  Visibility(
-                    visible: isRegisteredDriver,
-                    child: const ListTile(
-                      title: Text('Rating'),
-                      leading: Icon(
-                        Icons.star,
-                        color: Colors.amber,
-                      ),
-                      trailing: Text('4.8'),
-                    ),
-                  ),
-                  const ListTile(
-                    title: Text('Points'),
-                    leading: Icon(Icons.score),
-                    trailing: Text('58'),
-                  ),
-                  ExpansionTile(
-                    title: const Text('My Locations'),
-                    leading: const Icon(Icons.map),
-                    children: [
-                      SizedBox(
-                        height: constraints.maxHeight * 0.1,
-                        child: ListView(
-                          scrollDirection: Axis.horizontal,
-                          padding: EdgeInsets.symmetric(
-                            vertical: constraints.maxHeight * 0.025,
-                            horizontal: constraints.maxWidth * 0.025,
-                          ),
-                          children: [
-                            Container(
-                              margin: EdgeInsets.symmetric(
-                                horizontal: constraints.maxWidth * 0.025,
-                              ),
-                              width: constraints.maxWidth * 0.3,
-                              child: ElevatedButton(
-                                onPressed: () {},
-                                child: const Text('Home'),
-                                style: ElevatedButton.styleFrom(
-                                  shape: RoundedRectangleBorder(
-                                    borderRadius: BorderRadius.circular(24),
-                                  ),
-                                ),
-                              ),
-                            ),
-                            Container(
-                              margin: EdgeInsets.symmetric(
-                                horizontal: constraints.maxWidth * 0.025,
-                              ),
-                              width: constraints.maxWidth * 0.3,
-                              child: ElevatedButton(
-                                onPressed: () {},
-                                child: Text(isStudent ? 'School' : 'Work'),
-                                style: ElevatedButton.styleFrom(
-                                  shape: RoundedRectangleBorder(
-                                    borderRadius: BorderRadius.circular(24),
-                                  ),
-                                ),
-                              ),
-                            ),
-                            Container(
-                              margin: EdgeInsets.symmetric(
-                                horizontal: constraints.maxWidth * 0.025,
-                              ),
-                              width: constraints.maxWidth * 0.3,
-                              child: ElevatedButton(
-                                onPressed: () {},
-                                child: const Icon(Icons.add),
-                                style: ElevatedButton.styleFrom(
-                                  primary: Colors.grey[400],
-                                  shape: RoundedRectangleBorder(
-                                    borderRadius: BorderRadius.circular(24),
-                                  ),
-                                ),
-                              ),
-                            ),
-                          ],
+              child: SingleChildScrollView(
+                scrollDirection: Axis.vertical,
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Visibility(
+                      visible: isRegisteredDriver,
+                      child: const ListTile(
+                        title: Text('Rating'),
+                        leading: Icon(
+                          Icons.star,
+                          color: Colors.amber,
                         ),
-                      )
-                    ],
-                  ),
-                  isRegisteredDriver
-                      ? ExpansionTile(
-                          leading: Icon(Icons.drive_eta),
-                          title: const Text(
-                            'Owned Cars',
-                          ),
-                          children: [
-                            SizedBox(
-                              height: constraints.maxHeight * 0.25,
-                              child: SingleChildScrollView(
-                                scrollDirection: Axis.horizontal,
-                                child: Row(
-                                  children: [
-                                    Container(
-                                      margin: EdgeInsets.symmetric(
-                                        horizontal:
-                                            constraints.maxWidth * 0.025,
-                                      ),
-                                      height: constraints.maxHeight * 0.2,
-                                      width: constraints.maxWidth * 0.3,
-                                      decoration: BoxDecoration(
-                                        borderRadius: BorderRadius.circular(12),
-                                        color: Colors.purple[200],
-                                      ),
-                                      child: Column(
-                                        mainAxisAlignment:
-                                            MainAxisAlignment.center,
-                                        children: const [
-                                          Icon(CupertinoIcons.car),
-                                          Text('FAG 164'),
-                                          Text('Suzuki Ertiga'),
-                                        ],
-                                      ),
+                        trailing: Text('4.8'),
+                      ),
+                    ),
+                    const ListTile(
+                      title: Text('Points'),
+                      leading: Icon(Icons.score),
+                      trailing: Text('58'),
+                    ),
+                    ExpansionTile(
+                      title: const Text('My Locations'),
+                      leading: const Icon(Icons.map),
+                      children: [
+                        SizedBox(
+                          height: constraints.maxHeight * 0.1,
+                          child: ListView(
+                            scrollDirection: Axis.horizontal,
+                            padding: EdgeInsets.symmetric(
+                              vertical: constraints.maxHeight * 0.025,
+                              horizontal: constraints.maxWidth * 0.025,
+                            ),
+                            children: [
+                              Container(
+                                margin: EdgeInsets.symmetric(
+                                  horizontal: constraints.maxWidth * 0.025,
+                                ),
+                                width: constraints.maxWidth * 0.3,
+                                child: ElevatedButton(
+                                  onPressed: () {},
+                                  child: const Text('Home'),
+                                  style: ElevatedButton.styleFrom(
+                                    shape: RoundedRectangleBorder(
+                                      borderRadius: BorderRadius.circular(24),
                                     ),
-                                  ],
+                                  ),
                                 ),
                               ),
-                            ),
-                          ],
+                              Container(
+                                margin: EdgeInsets.symmetric(
+                                  horizontal: constraints.maxWidth * 0.025,
+                                ),
+                                width: constraints.maxWidth * 0.3,
+                                child: ElevatedButton(
+                                  onPressed: () {},
+                                  child: Text(isStudent ? 'School' : 'Work'),
+                                  style: ElevatedButton.styleFrom(
+                                    shape: RoundedRectangleBorder(
+                                      borderRadius: BorderRadius.circular(24),
+                                    ),
+                                  ),
+                                ),
+                              ),
+                              Container(
+                                margin: EdgeInsets.symmetric(
+                                  horizontal: constraints.maxWidth * 0.025,
+                                ),
+                                width: constraints.maxWidth * 0.3,
+                                child: ElevatedButton(
+                                  onPressed: () {},
+                                  child: const Icon(Icons.add),
+                                  style: ElevatedButton.styleFrom(
+                                    primary: Colors.grey[400],
+                                    shape: RoundedRectangleBorder(
+                                      borderRadius: BorderRadius.circular(24),
+                                    ),
+                                  ),
+                                ),
+                              ),
+                            ],
+                          ),
                         )
-                      : GestureDetector(
-                          onTap: model.gotoDriverRegisterView,
-                          child: ListTile(
+                      ],
+                    ),
+                    isRegisteredDriver
+                        ? ExpansionTile(
                             leading: Icon(Icons.drive_eta),
                             title: const Text(
-                              'Register as Driver',
-                              style: TextStyle(color: Colors.blue),
+                              'Owned Cars',
+                            ),
+                            children: [
+                              SizedBox(
+                                height: constraints.maxHeight * 0.25,
+                                child: SingleChildScrollView(
+                                  scrollDirection: Axis.horizontal,
+                                  child: Row(
+                                    children: [
+                                      Container(
+                                        margin: EdgeInsets.symmetric(
+                                          horizontal:
+                                              constraints.maxWidth * 0.025,
+                                        ),
+                                        height: constraints.maxHeight * 0.2,
+                                        width: constraints.maxWidth * 0.3,
+                                        decoration: BoxDecoration(
+                                          borderRadius:
+                                              BorderRadius.circular(12),
+                                          color: Colors.purple[200],
+                                        ),
+                                        child: Column(
+                                          mainAxisAlignment:
+                                              MainAxisAlignment.center,
+                                          children: const [
+                                            Icon(CupertinoIcons.car),
+                                            Text('FAG 164'),
+                                            Text('Suzuki Ertiga'),
+                                          ],
+                                        ),
+                                      ),
+                                    ],
+                                  ),
+                                ),
+                              ),
+                            ],
+                          )
+                        : GestureDetector(
+                            onTap: model.gotoDriverRegisterView,
+                            child: ListTile(
+                              leading: Icon(Icons.drive_eta),
+                              title: const Text(
+                                'Register as Driver',
+                                style: TextStyle(color: Colors.blue),
+                              ),
                             ),
                           ),
-                        ),
-                  Spacer(),
-                  GestureDetector(
-                    onTap: model.signOut,
-                    child: ListTile(
-                      title: Text('Logout'),
-                      leading: Icon(Icons.logout_outlined),
-                    ),
-                  )
-                ],
+                    GestureDetector(
+                      onTap: model.signOut,
+                      child: ListTile(
+                        title: Text('Logout'),
+                        leading: Icon(Icons.logout_outlined),
+                      ),
+                    )
+                  ],
+                ),
               ),
             )
           ],
