c69b6ec24f04c98327dc4b1aa3271353e8a9a724
Paolo Pepito <npaolopepito@gmail.com>
2021-10-06 21:08:52 +0800

    Add currentUserInfoProvider as a core provider

--- lib/data/core/core_providers.dart
+++ lib/data/core/core_providers.dart
@@ -1,6 +1,10 @@
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
+import 'package:kapiot/data/repositories/user_info_repository.dart';
 import 'package:kapiot/model/kapiot_user/kapiot_user.dart';
+import 'package:kapiot/model/kapiot_user_info/kapiot_user_info.dart';
+
+final fireauthProvider = Provider<FirebaseAuth>((ref) => FirebaseAuth.instance);
 
 final authStateChangesProvider = StreamProvider<User?>(
     (ref) => ref.watch(fireauthProvider).authStateChanges());
@@ -20,4 +24,18 @@ final currentUserProvider = Provider.autoDispose<KapiotUser?>((ref) {
   }
 });
 
-final fireauthProvider = Provider<FirebaseAuth>((ref) => FirebaseAuth.instance);
+final currentUserInfoProvider = StreamProvider.autoDispose<KapiotUserInfo?>(
+  (ref) async* {
+    final currentUser = ref.watch(currentUserProvider);
+    if (currentUser != null) {
+      final userInfoStream = ref
+          .watch(userInfoRepositoryProvider)
+          .getUserInfoStream(currentUser.id);
+      await for (final userInfo in userInfoStream) {
+        yield userInfo;
+      }
+    } else {
+      yield null;
+    }
+  },
+);
