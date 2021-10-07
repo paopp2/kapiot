e60601933de6d48070631df4071592502b998f21
crisxchan <chrisgonzales.online@gmail.com>
2021-10-07 23:30:37 +0800

    Add SavePlaceView as final view in saving place

--- lib/ui/place/place_manager_view.dart
+++ lib/ui/place/place_manager_view.dart
@@ -8,102 +8,105 @@ class PlaceManagerView extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return SafeArea(
-      child: LayoutBuilder(builder: (context, constraints) {
-        return Scaffold(
-          appBar: AppBar(
-            backgroundColor: Colors.white,
-            elevation: 0,
-            leading: IconButton(
-              onPressed: AppRouter.instance.popView,
-              icon: const Icon(
-                CupertinoIcons.arrow_left,
-                color: Colors.blue,
+      child: LayoutBuilder(
+        builder: (context, constraints) {
+          return Scaffold(
+            backgroundColor: const Color(0xffeeeeee),
+            appBar: AppBar(
+              backgroundColor: Colors.white,
+              elevation: 0,
+              leading: IconButton(
+                onPressed: AppRouter.instance.popView,
+                icon: const Icon(
+                  CupertinoIcons.arrow_left,
+                  color: Colors.blue,
+                ),
               ),
             ),
-          ),
-          body: Container(
-            color: Color(0xffeeeeee),
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-                Padding(
-                  padding: EdgeInsets.symmetric(
-                    vertical: constraints.maxHeight * 0.025,
-                    horizontal: constraints.maxWidth * 0.05,
+            body: Container(
+              color: const Color(0xffeeeeee),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Padding(
+                    padding: EdgeInsets.symmetric(
+                      vertical: constraints.maxHeight * 0.025,
+                      horizontal: constraints.maxWidth * 0.05,
+                    ),
+                    child: const Text('Saved Places'),
                   ),
-                  child: Text('Saved Places'),
-                ),
-                Container(
-                  color: Colors.white,
-                  child: Column(
-                    children: const [
-                      SizedBox(
-                        height: 80,
-                        child: Center(
-                          child: ListTile(
-                            leading: Icon(Icons.home),
-                            title: Text('Home'),
-                            subtitle: Text(
-                              'Sunlight Drive, Sunny Hills Subdivision, Talamban, Cebu City, Philippines',
-                              overflow: TextOverflow.ellipsis,
+                  Container(
+                    color: Colors.white,
+                    child: Column(
+                      children: const [
+                        SizedBox(
+                          height: 80,
+                          child: Center(
+                            child: ListTile(
+                              leading: Icon(Icons.home),
+                              title: Text('Home'),
+                              subtitle: Text(
+                                'Sunlight Drive, Sunny Hills Subdivision, Talamban, Cebu City, Philippines',
+                                overflow: TextOverflow.ellipsis,
+                              ),
+                              trailing: Icon(Icons.edit),
                             ),
-                            trailing: Icon(Icons.edit),
                           ),
                         ),
-                      ),
-                      Divider(),
-                      SizedBox(
-                        height: 80,
-                        child: Center(
-                          child: ListTile(
-                            leading: Icon(Icons.work),
-                            title: Text('Work'),
-                            trailing: Icon(Icons.edit),
+                        Divider(),
+                        SizedBox(
+                          height: 80,
+                          child: Center(
+                            child: ListTile(
+                              leading: Icon(Icons.work),
+                              title: Text('Work'),
+                              trailing: Icon(Icons.edit),
+                            ),
                           ),
                         ),
-                      ),
-                      Divider(),
-                      SizedBox(
-                        height: 80,
-                        child: Center(
-                          child: ListTile(
-                            leading: Icon(Icons.add),
-                            title: Text('Add New'),
+                        Divider(),
+                        SizedBox(
+                          height: 80,
+                          child: Center(
+                            child: ListTile(
+                              leading: Icon(Icons.add),
+                              title: Text('Add New'),
+                            ),
                           ),
                         ),
-                      ),
-                    ],
+                      ],
+                    ),
                   ),
-                ),
-                Padding(
-                  padding: EdgeInsets.symmetric(
-                    vertical: constraints.maxHeight * 0.025,
-                    horizontal: constraints.maxWidth * 0.05,
+                  Padding(
+                    padding: EdgeInsets.symmetric(
+                      vertical: constraints.maxHeight * 0.025,
+                      horizontal: constraints.maxWidth * 0.05,
+                    ),
+                    child: const Text('Recently Used'),
                   ),
-                  child: Text('Recently Used'),
-                ),
-                Container(
-                  color: Colors.white,
-                  child: const SizedBox(
-                    height: 80,
-                    child: Center(
-                      child: ListTile(
-                        leading: Icon(Icons.access_time_rounded),
-                        title: Text('University of San Carlos'),
-                        subtitle: Text(
-                          'Sunlight Drive, Sunny Hills Subdivision, Talamban, Cebu City, Philippines',
-                          overflow: TextOverflow.ellipsis,
+                  Container(
+                    color: Colors.white,
+                    child: const SizedBox(
+                      height: 80,
+                      child: Center(
+                        child: ListTile(
+                          leading: Icon(Icons.access_time_rounded),
+                          title: Text('University of San Carlos'),
+                          subtitle: Text(
+                            'Sunlight Drive, Sunny Hills Subdivision, Talamban, Cebu City, Philippines',
+                            overflow: TextOverflow.ellipsis,
+                          ),
+                          trailing: Icon(Icons.bookmark_outline),
                         ),
-                        trailing: Icon(Icons.bookmark_outline),
                       ),
                     ),
                   ),
-                ),
-              ],
+                ],
+              ),
             ),
-          ),
-        );
-      }),
+          );
+        },
+      ),
     );
   }
 }
