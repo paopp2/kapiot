6ca48d63e1a86df373a892162b54e984ec7fce12
Charles Jr Ausejo <charlesjrausejo@gmail.com>
2021-10-07 19:13:04 +0800

    WIP: Push updated KapiotUser

--- lib/logic/user_info/edit_user_info_view_model.dart
+++ lib/logic/user_info/edit_user_info_view_model.dart
@@ -1,4 +1,5 @@
 import 'package:hooks_riverpod/hooks_riverpod.dart';
+import 'package:kapiot/data/core/core_providers.dart';
 import 'package:kapiot/data/repositories/user_info_repository.dart';
 import 'package:kapiot/logic/shared/view_model.dart';
 import 'package:kapiot/logic/user_info/edit_user_info_state.dart';
@@ -7,6 +8,7 @@ import 'package:kapiot/model/kapiot_user/kapiot_user.dart';
 final editUserInfoViewModel = Provider.autoDispose(
   (ref) => EditUserInfoViewModel(
     read: ref.read,
+    currentUser: ref.watch(currentUserProvider),
     userInfoRepo: ref.watch(userInfoRepositoryProvider),
   ),
 );
@@ -14,8 +16,10 @@ final editUserInfoViewModel = Provider.autoDispose(
 class EditUserInfoViewModel extends ViewModel {
   EditUserInfoViewModel({
     required Reader read,
+    required this.currentUser,
     required this.userInfoRepo,
   }) : super(read);
+  final KapiotUser? currentUser;
   final UserInfoRepository userInfoRepo;
 
   @override
@@ -24,5 +28,12 @@ class EditUserInfoViewModel extends ViewModel {
   void setUserType(UserType userType) {
     read(userTypeProvider).state = userType;
     print(read(userTypeProvider).state);
+  }
+
+  void updateUserInfo() {
+    print(read(userTypeProvider).state);
+    final updatedUser =
+        currentUser!.copyWith(userType: read(userTypeProvider).state);
+    print(updatedUser);
   }
 }
