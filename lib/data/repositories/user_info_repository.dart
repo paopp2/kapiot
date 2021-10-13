5ed37fc8c2f3720cdca84fbf8f539b8cc25ad5c3
Charles Jr Ausejo <charlesjrausejo@gmail.com>
2021-10-13 14:34:13 +0800

    Add function that will return a Stream of DriverInfos

--- lib/data/repositories/user_info_repository.dart
+++ lib/data/repositories/user_info_repository.dart
@@ -1,6 +1,7 @@
 import 'package:hooks_riverpod/hooks_riverpod.dart';
 import 'package:kapiot/data/helpers/firestore_helper.dart';
 import 'package:kapiot/data/helpers/firestore_path.dart';
+import 'package:kapiot/model/driver_info/driver_info.dart';
 import 'package:kapiot/model/kapiot_user_info/kapiot_user_info.dart';
 
 final userInfoRepositoryProvider = Provider.autoDispose(
@@ -27,6 +28,13 @@ class UserInfoRepository {
     return firestoreHelper.documentStream(
       path: FirestorePath.docUserInfo(userId),
       builder: (data, _) => KapiotUserInfo.fromJson(data),
+    );
+  }
+
+  Stream<DriverInfo?> getDriverInfoStream(String driverId) {
+    return firestoreHelper.documentStream(
+      path: FirestorePath.docDriverInfo(driverId),
+      builder: (data, _) => DriverInfo.fromJson(data),
     );
   }
 }
