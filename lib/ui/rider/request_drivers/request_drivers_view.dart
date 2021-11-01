92132345c855418ecd19078f2b2a3795f9fdad04
Paolo Pepito <npaolopepito@gmail.com>
2021-11-01 23:27:01 +0800

    Final changes before merging into develop

--- lib/ui/rider/request_drivers/request_drivers_view.dart
+++ lib/ui/rider/request_drivers/request_drivers_view.dart
@@ -2,7 +2,6 @@ import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_hooks/flutter_hooks.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
-import 'package:kapiot/app_router.dart';
 import 'package:kapiot/logic/rider/request_drivers/request_drivers_view_model.dart';
 import 'package:kapiot/ui/rider/request_drivers/components/driver_card_stream.dart';
 import 'package:kapiot/ui/rider/request_drivers/components/request_drivers_view_map.dart';
