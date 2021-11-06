51748c126b17b742510bafed72a3e1deaa38cb67
crisxchan <chrisgonzales.online@gmail.com>
2021-11-06 20:44:25 +0800

    Finish design for RideInfoPanel

--- lib/ui/rider/request_accepted/components/ride_info_panel.dart
+++ lib/ui/rider/request_accepted/components/ride_info_panel.dart
@@ -1,6 +1,9 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/painting.dart';
 import 'package:flutter/widgets.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+import 'package:google_fonts/google_fonts.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
 import 'package:kapiot/logic/rider/request_accepted/request_accepted_view_model.dart';
 import 'package:kapiot/logic/rider/request_accepted/request_accepted_view_state.dart';
@@ -34,13 +37,13 @@ class RideInfoPanel extends HookConsumerWidget {
         child: Column(
           children: [
             Container(
-              alignment: Alignment.bottomCenter,
-              height: constraints.maxHeight * 0.05,
+              alignment: Alignment.bottomLeft,
+              height: constraints.maxHeight * 0.08,
               child: Text(
                 '${driverName.firstName} is arriving in $estTimeArrival',
-                style: const TextStyle(
-                  fontSize: 16,
-                  fontWeight: FontWeight.w500,
+                style: GoogleFonts.montserrat(
+                  fontSize: 17,
+                  color: const Color(0xff333333),
                 ),
               ),
             ),
@@ -52,10 +55,12 @@ class RideInfoPanel extends HookConsumerWidget {
               child: Row(
                 children: [
                   Container(
-                    margin: EdgeInsets.only(right: constraints.maxWidth * 0.02),
+                    margin: EdgeInsets.only(
+                      right: constraints.maxWidth * 0.025,
+                    ),
                     child: Material(
                       shape: RoundedRectangleBorder(
-                        borderRadius: BorderRadius.circular(10),
+                        borderRadius: BorderRadius.circular(6),
                       ),
                       elevation: 2.5,
                       child: Container(
@@ -71,85 +76,104 @@ class RideInfoPanel extends HookConsumerWidget {
                     ),
                   ),
                   Expanded(
-                    child: SizedBox(
-                      height: 60,
-                      child: Row(
-                        children: [
-                          Expanded(
-                            child: Column(
-                              mainAxisAlignment: MainAxisAlignment.spaceAround,
-                              crossAxisAlignment: CrossAxisAlignment.start,
-                              children: [
-                                SizedBox(
-                                  height: 21,
-                                  child: SingleChildScrollView(
-                                    scrollDirection: Axis.horizontal,
-                                    child: Text(
-                                      '${driverName.firstName} ${driverName.lastName}',
-                                      style: const TextStyle(
-                                        fontSize: 17,
-                                        fontWeight: FontWeight.bold,
-                                      ),
-                                    ),
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: [
+                        Column(
+                          mainAxisAlignment: MainAxisAlignment.center,
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: [
+                            SizedBox(
+                              height: 21,
+                              child: SingleChildScrollView(
+                                scrollDirection: Axis.horizontal,
+                                child: Text(
+                                  '${driverName.firstName} ${driverName.lastName}',
+                                  style: GoogleFonts.montserrat(
+                                    fontSize: 17,
+                                    color: const Color(0xff333333),
+                                    fontWeight: FontWeight.w500,
                                   ),
                                 ),
-                                const Text(
-                                  'Suzuki Ertiga',
-                                  style: TextStyle(
-                                    fontSize: 16,
-                                    color: Colors.grey,
+                              ),
+                            ),
+                            Row(
+                              mainAxisSize: MainAxisSize.min,
+                              children: [
+                                const Icon(
+                                  Icons.star_rate,
+                                  size: 18,
+                                  color: Color(0xFFFDCC0D),
+                                ),
+                                const SizedBox(
+                                  width: 5,
+                                ),
+                                Text(
+                                  '4.8',
+                                  style: GoogleFonts.montserrat(
+                                    fontSize: 12,
+                                    color: const Color(0xFFAAAAAA),
                                   ),
                                 )
                               ],
                             ),
-                          ),
-                          SizedBox(
-                            width: constraints.maxWidth * 0.2,
-                            child: Column(
-                              mainAxisAlignment: MainAxisAlignment.spaceAround,
-                              crossAxisAlignment: CrossAxisAlignment.start,
+                            Row(
+                              mainAxisSize: MainAxisSize.min,
                               children: [
-                                Row(
-                                  mainAxisSize: MainAxisSize.min,
-                                  children: [
-                                    Container(
-                                      margin: EdgeInsets.only(
-                                        right: constraints.maxWidth * 0.02,
-                                      ),
-                                      child: const Icon(
-                                        Icons.star,
-                                        color: Colors.amber,
-                                      ),
-                                    ),
-                                    const Text(
-                                      '4.8',
-                                      style: TextStyle(fontSize: 12),
-                                    )
-                                  ],
+                                const Icon(
+                                  CupertinoIcons.location,
+                                  size: 18,
+                                  color: Color(0xFFAAAAAA),
                                 ),
-                                Row(
-                                  mainAxisSize: MainAxisSize.min,
-                                  children: [
-                                    Container(
-                                      margin: EdgeInsets.only(
-                                        right: constraints.maxWidth * 0.02,
-                                      ),
-                                      child: const Icon(
-                                        Icons.location_on,
-                                        color: Colors.grey,
-                                      ),
-                                    ),
-                                    const Text(
-                                      '9 mins',
-                                      style: TextStyle(fontSize: 12),
-                                    )
-                                  ],
+                                const SizedBox(
+                                  width: 5,
+                                ),
+                                Text(
+                                  '9 mins',
+                                  style: GoogleFonts.montserrat(
+                                    fontSize: 12,
+                                    color: const Color(0xFFAAAAAA),
+                                  ),
                                 )
                               ],
                             ),
-                          ),
-                        ],
-                      ),
+                          ],
+                        ),
+                        Column(
+                          mainAxisAlignment: MainAxisAlignment.end,
+                          crossAxisAlignment: CrossAxisAlignment.end,
+                          children: [
+                            const Image(
+                              image: AssetImage(
+                                  'assets/images/car_body_types/coupe.png'),
+                              height: 50,
+                            ),
+                            RichText(
+                              text: TextSpan(
+                                style: const TextStyle(fontSize: 12),
+                                children: [
+                                  TextSpan(
+                                    text: 'Toyota Fortuner - ',
+                                    style: GoogleFonts.montserrat(
+                                      color: const Color(0xFFAAAAAA),
+                                      fontSize: 12,
+                                    ),
+                                  ),
+                                  TextSpan(
+                                    text: 'AAA 111',
+                                    style: GoogleFonts.montserrat(
+                                      color: const Color(0xFF333333),
+                                      fontSize: 12,
+                                    ),
+                                  )
+                                ],
+                              ),
+                              overflow: TextOverflow.fade,
+                              softWrap: false,
+                            ),
+                          ],
+                        )
+                      ],
                     ),
                   )
                 ],
@@ -161,9 +185,13 @@ class RideInfoPanel extends HookConsumerWidget {
               children: [
                 Container(
                   margin: EdgeInsets.only(bottom: constraints.maxHeight * 0.02),
-                  child: const Text(
-                    'PASSENGERS',
-                    style: TextStyle(fontWeight: FontWeight.bold),
+                  child: Text(
+                    'Passengers',
+                    style: GoogleFonts.montserrat(
+                      fontSize: 15,
+                      color: const Color(0xff333333),
+                      fontWeight: FontWeight.w500,
+                    ),
                   ),
                 ),
                 Column(
@@ -201,8 +229,13 @@ class RideInfoPanel extends HookConsumerWidget {
                       ),
                     ),
                     Text(
-                      'You and $coRiderCount other people',
-                      style: const TextStyle(color: Colors.grey),
+                      coRiderConfigsStream.data!.value.isEmpty
+                          ? 'You'
+                          : 'You and $coRiderCount other people',
+                      style: GoogleFonts.montserrat(
+                        fontSize: 12,
+                        color: const Color(0xFFAAAAAA),
+                      ),
                     ),
                   ],
                 ),
@@ -214,22 +247,27 @@ class RideInfoPanel extends HookConsumerWidget {
                 margin: EdgeInsets.only(bottom: constraints.maxHeight * 0.015),
                 child: Column(
                   mainAxisAlignment: MainAxisAlignment.center,
+                  crossAxisAlignment: CrossAxisAlignment.center,
                   children: [
                     Container(
                       margin: EdgeInsets.only(
                         bottom: constraints.maxHeight * 0.005,
                       ),
-                      child: const Text(
+                      child: Text(
                         '10.0 points',
-                        style: TextStyle(
-                          fontSize: 18,
-                          fontWeight: FontWeight.bold,
+                        style: GoogleFonts.montserrat(
+                          fontSize: 15,
+                          color: const Color(0xff333333),
+                          fontWeight: FontWeight.w500,
                         ),
                       ),
                     ),
-                    const Text(
+                    Text(
                       'Ride Reward',
-                      style: TextStyle(color: Colors.grey),
+                      style: GoogleFonts.montserrat(
+                        fontSize: 12,
+                        color: const Color(0xFFAAAAAA),
+                      ),
                     ),
                   ],
                 ),
