88b65e065282f4bf2e949c78f7b90cb91345ddd7
crisxchan <chrisgonzales.online@gmail.com>
2021-11-08 14:29:23 +0800

    Initial branch commit

--- lib/ui/rider/request_drivers/components/driver_card_stream.dart
+++ lib/ui/rider/request_drivers/components/driver_card_stream.dart
@@ -5,6 +5,7 @@ import 'package:hooks_riverpod/hooks_riverpod.dart';
 import 'package:kapiot/logic/rider/request_drivers/request_drivers_view_model.dart';
 import 'package:kapiot/logic/rider/request_drivers/request_drivers_view_state.dart';
 import 'package:kapiot/model/route_config/route_config.dart';
+import 'package:kapiot/ui/shared/loading_widget.dart';
 
 import 'driver_card.dart';
 
@@ -20,68 +21,61 @@ class DriverCardStream extends HookConsumerWidget {
   Widget build(BuildContext context, WidgetRef ref) {
     final compatibleDriverConfigStream =
         ref.watch(compatibleDriverConfigStreamProvider);
-    return Builder(builder: (context) {
-      return compatibleDriverConfigStream.when(
-        error: (_, __) => const Center(
-          child: Text('Error'),
-        ),
-        loading: () => Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            Image(
-              image: const AssetImage('assets/images/error/no_drivers.png'),
-              height: constraints.maxHeight * 0.15,
-            ),
-            const SizedBox(
-              height: 20,
-            ),
-            SizedBox(
-              child: Column(
-                children: [
-                  Text(
-                    'No Drivers Found',
-                    style: GoogleFonts.poppins(
-                      fontSize: 24,
-                      fontWeight: FontWeight.bold,
-                      color: const Color(0xFF333333),
+    return Builder(
+      builder: (context) {
+        return compatibleDriverConfigStream.when(
+          error: (_, __) => const Center(
+            child: Text('Error'),
+          ),
+          loading: () => LoadingWidget(),
+          data: (compatibleDriverConfigs) {
+            return compatibleDriverConfigs.isEmpty
+                ? SizedBox(
+                    child: Column(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        Text(
+                          'No Drivers Found',
+                          style: GoogleFonts.poppins(
+                            fontSize: 24,
+                            fontWeight: FontWeight.bold,
+                            color: const Color(0xFF333333),
+                          ),
+                        ),
+                        Text(
+                          "When you have compatible drivers along your\nroute, you'll see them here.",
+                          style: GoogleFonts.montserrat(
+                            fontSize: 12,
+                            color: const Color(0xFFAAAAAA),
+                            height: 1.5,
+                          ),
+                          textAlign: TextAlign.center,
+                        )
+                      ],
                     ),
-                  ),
-                  Text(
-                    "When you have compatible drivers along your\nroute, you'll see them here.",
-                    style: GoogleFonts.montserrat(
-                      fontSize: 12,
-                      color: const Color(0xFFAAAAAA),
-                      height: 1.5,
-                    ),
-                    textAlign: TextAlign.center,
                   )
-                ],
-              ),
-            ),
-          ],
-        ),
-        data: (compatibleDriverConfigs) {
-          return CarouselSlider.builder(
-            itemCount: compatibleDriverConfigs.length,
-            options: CarouselOptions(
-              autoPlay: false,
-              aspectRatio: 4 / 3,
-              viewportFraction: 0.7,
-              initialPage: 0,
-              enableInfiniteScroll: false,
-            ),
-            itemBuilder: (context, index, _) {
-              final driverConfig = compatibleDriverConfigs[index];
-              return DriverCard(
-                driverConfig: driverConfig as ForDriver,
-                model: model,
-                constraints: constraints,
-                index: index,
-              );
-            },
-          );
-        },
-      );
-    });
+                : CarouselSlider.builder(
+                    itemCount: compatibleDriverConfigs.length,
+                    options: CarouselOptions(
+                      autoPlay: false,
+                      aspectRatio: 4 / 3,
+                      viewportFraction: 0.7,
+                      initialPage: 0,
+                      enableInfiniteScroll: false,
+                    ),
+                    itemBuilder: (context, index, _) {
+                      final driverConfig = compatibleDriverConfigs[index];
+                      return DriverCard(
+                        driverConfig: driverConfig as ForDriver,
+                        model: model,
+                        constraints: constraints,
+                        index: index,
+                      );
+                    },
+                  );
+          },
+        );
+      },
+    );
   }
 }
