2c685cf6b9924657e7846f7a94acfdac51be95f6
Paolo Pepito <npaolopepito@gmail.com>
2021-11-14 16:22:13 +0800

    WIP: Add PS1 test cases

--- test/data/core/core_algo_point_system_test.dart
+++ test/data/core/core_algo_point_system_test.dart
@@ -1,92 +1,98 @@
 import 'dart:convert';
-import 'dart:io' as io;
+import 'dart:io';
 
 import 'package:flutter_dotenv/flutter_dotenv.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
 import 'package:kapiot/data/core/core_algorithms.dart';
+import 'package:kapiot/data/services/google_maps_api_services.dart';
+import 'package:kapiot/model/dist_matrix_element/dist_matrix_element.dart';
+import 'package:kapiot/model/kapiot_location/kapiot_location.dart';
 import 'package:kapiot/model/route_config/route_config.dart';
+import 'package:mockito/annotations.dart';
+import 'package:mockito/mockito.dart';
 
+import 'core_algo_point_system_test.mocks.dart';
+
+@GenerateMocks([DistanceMatrixService])
 Future<void> main() async {
   TestWidgetsFlutterBinding.ensureInitialized();
-  io.HttpOverrides.global = null;
   await dotenv.load(fileName: 'secrets/secrets.env');
-  test('PS1.1: Assuming driver from Pitogo Lacion to Pacific Mall', () async {
-    await verifyPointsAcceptable(
-      riders: ["Arthur", "Mother's Milk", "Frenchie"],
-      expectedPoints: 185.666663,
+  group('PS1: Single rider tests', () {
+    test('PS1.1.1: Assuming rider from Pitogo Lacion to Pacific Mall',
+        () async {
+      await verifyPoints(
+        assumedTravelDistance: 5123,
+        assumedTravelTime: 840,
+        expectedPoints: 96.306666,
+      );
+    });
+    test(
+      'PS1.1.2: Assuming rider from Mendero Medical Center to Co Jordan Farm and Fishing',
+      () async {
+        await verifyPoints(
+          assumedTravelDistance: 2755,
+          assumedTravelTime: 360,
+          expectedPoints: 48.733333,
+        );
+      },
     );
   });
-  test('PS1.2: Assuming driver from Pacific Mall to Pitogo Lacion', () async {
-    await verifyPointsAcceptable(
-      riders: ["R_Arthur", "R_Mother's Milk", "R_Frenchie"],
-      expectedPoints: 176.666663,
-    );
-  });
-  test(
-    "PS1.3: Assuming driver from Mcdo Lacion to Stephanie's Korean Restaurant",
-    () async {
-      await verifyPointsAcceptable(
-        riders: ["A-Train"],
-        expectedPoints: 103.333333,
-      );
-    },
-  );
-  test(
-    "PS1.4: Assuming driver from Stepanie's Korean Restaurant to Mcdo Lacion",
-    () async {
-      await verifyPointsAcceptable(
-        riders: ["R_A-Train"],
-        expectedPoints: 99,
-      );
-    },
-  );
-  test(
-    'PS1.5: Assuming driver from Seaoil Pitogo to Consolacion Community College',
-    () async {
-      await verifyPointsAcceptable(
-        riders: ["Jim Preston"],
-        expectedPoints: 33.333333,
-      );
-    },
-  );
 }
 
-/// Verifies that the points calculated by the system is within 5% of the
-/// manually calculated (expected) value
+/// Verifies that the [expectedPoints] matches the actual calculated value by
+/// the system based on the [assumedTravelDistance] and the
+/// [assumedTravelTime]
 ///
-/// Parameters used for the manual calculation of the expected value are derived
-/// from Google Maps' approximate time and distance of travel
-Future<void> verifyPointsAcceptable({
-  required List<String> riders,
+/// The 'assumed' parameters are the assumed values to be returned by the
+/// DistanceMatrixApi. The [assumedTravelDistance] should be in meters while the
+/// [assumedTravelTime] in seconds. The [expectedPoints] should have at least 3
+/// decimals
+Future<void> verifyPoints({
+  required double assumedTravelDistance,
+  required double assumedTravelTime,
   required double expectedPoints,
 }) async {
-  final container = ProviderContainer();
-  final coreAlgorithms = container.read(coreAlgorithmsProvider);
-  final ridersFile = io.File('functions/test_data/riders.json');
+  final mockDistMatrixService = MockDistanceMatrixService();
+  final ridersFile = File('functions/test_data/riders.json');
   final riderData = jsonDecode(await ridersFile.readAsString());
-  final ridersList = riderData['ridersList'] as List;
-  final riderConfigs = (ridersList.where(
-    (rider) {
-      rider as Map<String, dynamic>;
-      return riders.contains(rider['user']['displayName']);
-    },
-  )).toList();
+  final ridersList = riderData['ridersList'] as List<dynamic>;
+  final randomConfig = RouteConfig.fromJson(ridersList.first);
+  final container = ProviderContainer(
+    overrides: [
+      googleMapsApiServicesProvider.overrideWithValue(
+        GoogleMapsApiServices(
+          places: PlacesService.instance,
+          directions: DirectionsService.instance,
+          utils: MapsUtils.instance,
+          distMatrix: mockDistMatrixService,
+        ),
+      )
+    ],
+  );
+  final coreAlgorithms = container.read(coreAlgorithmsProvider);
 
-  double totalPoints = 0;
-  for (final rider in riderConfigs) {
-    totalPoints += await coreAlgorithms.calculateDriverPointsFromRider(
-      RouteConfig.fromJson(rider),
-    );
-  }
-  final percentError = ((totalPoints - expectedPoints).abs()) / expectedPoints;
-  // print(totalPoints);
-  // print(percentError);
-  expect(
-      percentError,
-      isA<double>().having(
-        (e) => (e < 0.05),
-        'Percent error less than 5%',
-        true,
+  when(mockDistMatrixService.getDistMatrixElement(
+    pointA: argThat(isA<KapiotLocation>(), named: 'pointA'),
+    pointB: argThat(isA<KapiotLocation>(), named: 'pointB'),
+  )).thenAnswer((_) async => DistMatrixElement(
+        distanceText: '',
+        distanceValue: assumedTravelDistance,
+        durationText: '',
+        durationValue: assumedTravelTime,
       ));
+
+  final totalPoints = await coreAlgorithms.calculateDriverPointsFromRider(
+    randomConfig,
+  );
+  expect(
+    totalPoints,
+    isA<double>().having(
+      (tp) {
+        return (tp.toStringAsFixed(2) == expectedPoints.toStringAsFixed(2));
+      },
+      'Actual and expected totalPoints are equal for up to 2 decimal places',
+      true,
+    ),
+  );
 }
