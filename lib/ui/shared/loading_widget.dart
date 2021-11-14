0c7e8ff319e83af2f98bcad7190fc5e0e81e49ab
crisxchan <chrisgonzales.online@gmail.com>
2021-11-14 18:01:22 +0800

    Update LoadingWidget using spinkit package

--- lib/ui/shared/loading_widget.dart
+++ lib/ui/shared/loading_widget.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_spinkit/flutter_spinkit.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 class LoadingWidget extends StatelessWidget {
   const LoadingWidget({
@@ -13,9 +15,20 @@ class LoadingWidget extends StatelessWidget {
       child: Column(
         mainAxisAlignment: MainAxisAlignment.center,
         children: [
-          const CircularProgressIndicator(),
+          const SpinKitWanderingCubes(
+            color: Color(0xFF5F45A4),
+            size: 50,
+          ),
           const SizedBox(height: 20),
-          Text(text ?? ''),
+          Text(
+            text ?? '',
+            textAlign: TextAlign.center,
+            style: GoogleFonts.montserrat(
+              fontSize: 12,
+              color: const Color(0xff666666),
+              height: 1.5,
+            ),
+          ),
         ],
       ),
     );
